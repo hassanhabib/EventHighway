6bb96554420c6c88e0309ce64ab5cdc9bd1f39d9
Hassan Habib <hassanhabib@live.com>
2024-11-03 03:48:40 -0800

    BROKERS: Initialize Storage Broker

---
+++ EventHighway.Core/Brokers/Storages/StorageBroker.cs
@@ -0,0 +1,20 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using EFxceptions;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventHighway.Core.Brokers.Storages
+{
+    internal partial class StorageBroker : EFxceptionsContext, IStorageBroker
+    {
+        private readonly string connectionString;
+
+        public StorageBroker(string connectionString) =>
+            this.connectionString = connectionString;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+             optionsBuilder.UseSqlServer(this.connectionString);
+    }
+}
