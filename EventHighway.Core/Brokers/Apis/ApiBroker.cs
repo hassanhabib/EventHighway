67f8a423172f71dddf708e6ce2ccf5e5c09600ea
Hassan Rezk Habib <hassanhabib@live.com>
2024-11-03 15:39:03 -0800

    BROKERS: API Broker

---
+++ EventHighway.Core/Brokers/Apis/ApiBroker.cs
@@ -0,0 +1,34 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventHighway.Core.Brokers.Apis
+{
+    internal class ApiBroker : IApiBroker
+    {
+        private readonly HttpClient httpClient;
+
+        public ApiBroker() =>
+            this.httpClient = new HttpClient();
+
+        public async ValueTask<string> PostAsync(string content, string url)
+        {
+            var stringContent =
+               new StringContent(
+                   content,
+                   encoding: Encoding.UTF8,
+                   mediaType: "application/json");
+
+            HttpResponseMessage httpResponseMessage =
+                await this.httpClient.PostAsync(
+                    requestUri: url,
+                    content: stringContent);
+
+            return httpResponseMessage.ToString();
+        }
+    }
+}
---
+++ EventHighway.Core/Brokers/Apis/IApiBroker.cs
@@ -0,0 +1,13 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using System.Threading.Tasks;
+
+namespace EventHighway.Core.Brokers.Apis
+{
+    internal interface IApiBroker
+    {
+        ValueTask<string> PostAsync(string content, string url);
+    }
+}
