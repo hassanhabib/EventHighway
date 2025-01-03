82ad2eb024d6c618f947922b3ea1eacb4acf8efe
Hassan Habib <hassanhabib@live.com>
2025-01-03 00:42:41 -0800

    MEDIUM FIX: Reinitialize Http Request For Each Call

--- EventHighway.Core/Brokers/Apis/ApiBroker.cs
+++ EventHighway.Core/Brokers/Apis/ApiBroker.cs
@@ -10,23 +10,20 @@ namespace EventHighway.Core.Brokers.Apis
 {
     internal class ApiBroker : IApiBroker
     {
-        private readonly HttpClient httpClient;
-
-        public ApiBroker() =>
-            this.httpClient = new HttpClient();
-
         public async ValueTask<string> PostAsync(string content, string url, string secret)
         {
+            var httpClient = new HttpClient();
+
             var stringContent =
                new StringContent(
                    content,
                    encoding: Encoding.UTF8,
                    mediaType: "application/json");
 
-            this.httpClient.DefaultRequestHeaders.Add("X-Highway", secret);
+            httpClient.DefaultRequestHeaders.Add("X-Highway", secret);
 
             HttpResponseMessage httpResponseMessage =
-                await this.httpClient.PostAsync(
+                await httpClient.PostAsync(
                     requestUri: url,
                     content: stringContent);
 
