0e1b7e1021e784824a10668b814fdab328681adb
ZafarUrakov <zafarurakov@outlook.com>
2025-02-26 17:15:16 +0800

    CODE RUB: Include Test Feature

--- EventHighway.App/Program.cs
+++ EventHighway.App/Program.cs
@@ -36,12 +36,40 @@ namespace EventHighway.App
             //    EventAddressId = Guid.Parse("d3b3b3b3-0b3b-4b3b-8b3b-0b3b3b3b3b32"),
             //});
 
+            //int numberOfEvents = 500;
+            //DateTimeOffset baseTime = DateTimeOffset.UtcNow;
+
+            //foreach (int i in Enumerable.Range(1, numberOfEvents))
+            //{
+            //    _ = eventHighwayClient.Events.SubmitEventAsync(new Core.Models.Events.Event
+            //    {
+            //        Content = $"{{ \"name\": \"Scheduled Test - {i}\" }}",
+            //        EventAddressId = Guid.Parse("d3b3b3b3-0b3b-4b3b-8b3b-0b3b3b3b3b32"),
+            //        Id = Guid.NewGuid(),
+            //        CreatedDate = DateTimeOffset.UtcNow,
+            //        UpdatedDate = DateTimeOffset.UtcNow,
+            //        PublishedDate = baseTime.AddSeconds(i * 1)
+            //    });
+            //}
+
+            //foreach (int i in Enumerable.Range(1, numberOfEvents - 400))
+            //{
+            //    await eventHighwayClient.Events.SubmitEventAsync(new Core.Models.Events.Event
+            //    {
+            //        Content = $"{{ \"name\": \"Immediate Test - {i}\" }}",
+            //        EventAddressId = Guid.Parse("d3b3b3b3-0b3b-4b3b-8b3b-0b3b3b3b3b32"),
+            //        Id = Guid.NewGuid(),
+            //        CreatedDate = DateTimeOffset.UtcNow,
+            //        UpdatedDate = DateTimeOffset.UtcNow
+            //    });
+            //}
+
             Console.WriteLine("Input seconds for the test 1:");
             int seconds = int.Parse(Console.ReadLine());
 
             Console.WriteLine("Input seconds for the test 2:");
             int seconds2 = int.Parse(Console.ReadLine());
-            
+
             Console.WriteLine("Input seconds for the test 4:");
             int seconds3 = int.Parse(Console.ReadLine());
 
@@ -64,7 +92,7 @@ namespace EventHighway.App
                 UpdatedDate = DateTimeOffset.UtcNow,
                 PublishedDate = DateTimeOffset.UtcNow.AddSeconds(seconds2),
             });
-            
+
             _ = eventHighwayClient.Events.SubmitEventAsync(new Core.Models.Events.Event
             {
                 Content = "{ \"name\": \"Test3\" }",
@@ -73,7 +101,7 @@ namespace EventHighway.App
                 CreatedDate = DateTimeOffset.UtcNow,
                 UpdatedDate = DateTimeOffset.UtcNow,
             });
-            
+
             _ = eventHighwayClient.Events.SubmitEventAsync(new Core.Models.Events.Event
             {
                 Content = "{ \"name\": \"Test4\" }",
