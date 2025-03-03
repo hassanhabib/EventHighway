664bacfff8494054fd0359a327a42f344773eb1c
ZafarUrakov <zafarurakov@outlook.com>
2025-03-03 15:21:40 +0800

    DATA: Add & MIgreate Event V2

---
+++ EventHighway.Core/Models/Events/V2/EventV2.cs
@@ -0,0 +1,18 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using System;
+
+namespace EventHighway.Core.Models.Events.V2
+{
+    public class EventV2
+    {
+        public Guid Id { get; set; }
+        public string Content { get; set; }
+        public EventV2Type Type { get; set; }
+        public DateTimeOffset CreatedDate { get; set; }
+        public DateTimeOffset UpdatedDate { get; set; }
+        public DateTimeOffset? ScheduledDate { get; set; }
+    }
+}
