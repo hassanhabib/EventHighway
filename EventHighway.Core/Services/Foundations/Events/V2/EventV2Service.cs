46521771df5091d15f2b93e1768510b737848895
ZafarUrakov <zafarurakov@outlook.com>
2025-03-04 11:53:47 +0800

    ShouldAddEventV2Async -> PASS

---
+++ EventHighway.Core/Services/Foundations/Events/V2/EventV2Service.cs
@@ -0,0 +1,21 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using System.Threading.Tasks;
+using EventHighway.Core.Brokers.Storages;
+using EventHighway.Core.Models.Events.V2;
+
+namespace EventHighway.Core.Services.Foundations.Events.V2
+{
+    internal partial class EventV2Service : IEventV2Service
+    {
+        private readonly IStorageBroker storageBroker;
+
+        public EventV2Service(IStorageBroker storageBroker) =>
+            this.storageBroker = storageBroker;
+
+        public async ValueTask<EventV2> AddEventV2Async(EventV2 eventV2) =>
+            await storageBroker.InsertEventV2Async(eventV2);
+    }
+}
