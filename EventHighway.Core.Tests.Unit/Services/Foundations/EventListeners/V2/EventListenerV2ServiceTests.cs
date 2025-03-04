b3cc90579056e03cde2989208785b85e350a0fab
ZafarUrakov <zafarurakov@outlook.com>
2025-03-04 15:23:47 +0800

    ShouldRetrieveAllEventListenerV2sAsync -> FAIL

---
+++ EventHighway.Core.Tests.Unit/Services/Foundations/EventListeners/V2/EventListenerV2ServiceTests.cs
@@ -0,0 +1,60 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Linq;
+using EventHighway.Core.Brokers.Loggings;
+using EventHighway.Core.Brokers.Storages;
+using EventHighway.Core.Models.EventListeners.V2;
+using EventHighway.Core.Services.Foundations.EventListeners.V2;
+using Moq;
+using Tynamix.ObjectFiller;
+
+namespace EventHighway.Core.Tests.Unit.Services.EventListeners.V2
+{
+    public partial class EventListenerV2ServiceTests
+    {
+        private readonly Mock<IStorageBroker> storageBrokerMock;
+        private readonly Mock<ILoggingBroker> loggingBrokerMock;
+        private readonly IEventListenerV2Service eventListenerV2Service;
+
+        public EventListenerV2ServiceTests()
+        {
+            this.storageBrokerMock = new Mock<IStorageBroker>();
+            this.loggingBrokerMock = new Mock<ILoggingBroker>();
+
+            this.eventListenerV2Service = new EventListenerV2Service(
+                storageBroker: this.storageBrokerMock.Object,
+                loggingBroker: this.loggingBrokerMock.Object);
+        }
+
+        private static EventListenerV2 CreateRandomEventListenerV2() =>
+            CreateEventListenerV2Filler().Create();
+
+        private static IQueryable<EventListenerV2> CreateRandomEventListenerV2s() =>
+            CreateEventListenerV2Filler().Create(count: GetRandomNumber()).AsQueryable();
+
+        private static int GetRandomNumber() =>
+            new IntRange(min: 2, max: 9).GetValue();
+
+        private static DateTimeOffset CreateRandomDateTime() =>
+            new DateTimeRange(earliestDate: DateTime.UnixEpoch).GetValue();
+
+        private static Filler<EventListenerV2> CreateEventListenerV2Filler()
+        {
+            var filler = new Filler<EventListenerV2>();
+
+            filler.Setup()
+                .OnType<DateTimeOffset>().Use(CreateRandomDateTime)
+
+                .OnProperty(eventListenerV2 =>
+                    eventListenerV2.EventAddress).IgnoreIt()
+
+                .OnProperty(eventListenerV2 =>
+                    eventListenerV2.ListenerEvents).IgnoreIt();
+
+            return filler;
+        }
+    }
+}
