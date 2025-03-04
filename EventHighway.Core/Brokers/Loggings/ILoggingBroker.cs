fec19439fd50a8111e1b156b4a07fbd0653f453e
ZafarUrakov <zafarurakov@outlook.com>
2025-03-04 12:04:41 +0800

    BROKERS: Log Error

---
+++ EventHighway.Core/Brokers/Loggings/ILoggingBroker.cs
@@ -0,0 +1,14 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Aspen Publishing. All rights reserved.
+// ---------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+
+namespace EventHighway.Core.Brokers.Loggings
+{
+    public interface ILoggingBroker
+    {
+        ValueTask LogErrorAsync(Exception exception);
+    }
+}
