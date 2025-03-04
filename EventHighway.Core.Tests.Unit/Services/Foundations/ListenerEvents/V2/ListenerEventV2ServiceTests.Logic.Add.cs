7ce7f95b762d7e9bbb12c22b90c3c049f6f1a154
ZafarUrakov <zafarurakov@outlook.com>
2025-03-04 16:26:48 +0800

    ShouldAddListenerEventV2Async -> FAIL

---
+++ EventHighway.Core.Tests.Unit/Services/Foundations/ListenerEvents/V2/ListenerEventV2ServiceTests.Logic.Add.cs
@@ -0,0 +1,54 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using System.Threading.Tasks;
+using EventHighway.Core.Models.ListenerEvents.V2;
+using FluentAssertions;
+using Force.DeepCloner;
+using Moq;
+
+namespace EventHighway.Core.Tests.Unit.Services.ListenerEvents.V2
+{
+    public partial class ListenerEventV2ServiceTests
+    {
+        [Fact]
+        public async Task ShouldAddListenerEventV2Async()
+        {
+            // given
+            ListenerEventV2 randomListenerEventV2 =
+                CreateRandomListenerEventV2();
+
+            ListenerEventV2 inputListenerEventV2 =
+                randomListenerEventV2;
+
+            ListenerEventV2 storageListenerEventV2 =
+                inputListenerEventV2;
+
+            ListenerEventV2 expectedListenerEventV2 =
+                storageListenerEventV2.DeepClone();
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.InsertListenerEventV2Async(
+                    inputListenerEventV2))
+                        .ReturnsAsync(storageListenerEventV2);
+
+            // when
+            ListenerEventV2 actualListenerEventV2 =
+                await this.listenerEventV2Service
+                    .AddListenerEventV2Async(
+                        inputListenerEventV2);
+
+            // then
+            actualListenerEventV2.Should().BeEquivalentTo(
+                expectedListenerEventV2);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertListenerEventV2Async(
+                    inputListenerEventV2),
+                        Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ EventHighway.Core.Tests.Unit/Services/Foundations/ListenerEvents/V2/ListenerEventV2ServiceTests.cs
@@ -0,0 +1,65 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using System;
+using EventHighway.Core.Brokers.Loggings;
+using EventHighway.Core.Brokers.Storages;
+using EventHighway.Core.Brokers.Times;
+using EventHighway.Core.Models.ListenerEvents.V2;
+using EventHighway.Core.Services.Foundations.ListernEvents.V2;
+using Moq;
+using Tynamix.ObjectFiller;
+
+namespace EventHighway.Core.Tests.Unit.Services.ListenerEvents.V2
+{
+    public partial class ListenerEventV2ServiceTests
+    {
+        private readonly Mock<IStorageBroker> storageBrokerMock;
+        private readonly Mock<IDateTimeBroker> dateTimeBrokerMock;
+        private readonly Mock<ILoggingBroker> loggingBrokerMock;
+        private readonly IListenerEventV2Service listenerEventV2Service;
+
+        public ListenerEventV2ServiceTests()
+        {
+            this.storageBrokerMock =
+                new Mock<IStorageBroker>();
+
+            this.loggingBrokerMock =
+                new Mock<ILoggingBroker>();
+
+            this.dateTimeBrokerMock =
+                new Mock<IDateTimeBroker>();
+
+            this.listenerEventV2Service = new ListenerEventV2Service(
+                storageBroker: this.storageBrokerMock.Object,
+                dateTimeBroker: this.dateTimeBrokerMock.Object,
+                loggingBroker: this.loggingBrokerMock.Object);
+        }
+
+        private static ListenerEventV2 CreateRandomListenerEventV2() =>
+            CreateListenerEventV2Filler().Create();
+
+        private static DateTimeOffset GetRandomDateTimeOffset() =>
+            new DateTimeRange(earliestDate: DateTime.UnixEpoch).GetValue();
+
+        private static Filler<ListenerEventV2> CreateListenerEventV2Filler()
+        {
+            var filler = new Filler<ListenerEventV2>();
+
+            filler.Setup()
+                .OnType<DateTimeOffset>().Use(GetRandomDateTimeOffset)
+
+                .OnProperty(listenerEventV2 => listenerEventV2.Event)
+                    .IgnoreIt()
+
+                .OnProperty(listenerEventV2 => listenerEventV2.EventAddress)
+                    .IgnoreIt()
+
+                .OnProperty(listenerEventV2 => listenerEventV2.EventListener)
+                    .IgnoreIt();
+
+            return filler;
+        }
+    }
+}
