9848d940ea7456745d052237ba961d9eda148756
ZafarUrakov <zafarurakov@outlook.com>
2025-03-05 14:16:48 +0800

    ShouldThrowDependencyValidationErrorOnModifyIfDatabaseUpdateConcurrencyOccursAndLogItAsync -> FAIL

--- EventHighway.Core.Tests.Unit/Services/Foundations/ListenerEvents/V2/ListenerEventV2ServiceTests.Exceptions.Modify.cs
+++ EventHighway.Core.Tests.Unit/Services/Foundations/ListenerEvents/V2/ListenerEventV2ServiceTests.Exceptions.Modify.cs
@@ -3,6 +3,7 @@
 // ----------------------------------------------------------------------------------
 
 using System;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using EventHighway.Core.Models.ListenerEvents.V2;
 using EventHighway.Core.Models.ListenerEvents.V2.Exceptions;
@@ -108,6 +109,58 @@ namespace EventHighway.Core.Tests.Unit.Services.Foundations.ListenerEvents.V2
             this.loggingBrokerMock.Verify(broker =>
                 broker.LogErrorAsync(It.Is(SameExceptionAs(
                     expectedListenerEventV2DependencyException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectListenerEventV2ByIdAsync(
+                    It.IsAny<Guid>()),
+                        Times.Never);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowDependencyValidationErrorOnModifyIfDatabaseUpdateConcurrencyOccursAndLogItAsync()
+        {
+            // given
+            ListenerEventV2 someListenerEventV2 = CreateRandomListenerEventV2();
+            var dbUpdateConcurrencyException = new DbUpdateConcurrencyException();
+
+            var lockedListenerEventV2Exception =
+                new LockedListenerEventV2Exception(
+                    message: "Listener event is locked, try again.",
+                    innerException: dbUpdateConcurrencyException);
+
+            var expectedListenerEventV2DependencyValidationException =
+                new ListenerEventV2DependencyValidationException(
+                    message: "Listener event validation error occurred, fix the errors and try again.",
+                    innerException: lockedListenerEventV2Exception);
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetDateTimeOffsetAsync())
+                    .ThrowsAsync(dbUpdateConcurrencyException);
+
+            // when
+            ValueTask<ListenerEventV2> modifyListenerEventV2Task =
+                this.listenerEventV2Service.ModifyListenerEventV2Async(someListenerEventV2);
+
+            ListenerEventV2DependencyValidationException actualListenerEventV2DependencyValidationException =
+                await Assert.ThrowsAsync<ListenerEventV2DependencyValidationException>(
+                    modifyListenerEventV2Task.AsTask);
+
+            // then
+            actualListenerEventV2DependencyValidationException.Should().BeEquivalentTo(
+                expectedListenerEventV2DependencyValidationException);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetDateTimeOffsetAsync(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedListenerEventV2DependencyValidationException))),
                         Times.Once);
 
             this.storageBrokerMock.Verify(broker =>
