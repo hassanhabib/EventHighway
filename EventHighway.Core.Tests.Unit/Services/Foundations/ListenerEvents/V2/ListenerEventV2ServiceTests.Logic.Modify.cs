69795f0a0f2e75029abfe37d246630a8cb651ecc
ZafarUrakov <zafarurakov@outlook.com>
2025-03-05 11:41:58 +0800

    ShouldModifyListenerEventV2Async -> FAIL

---
+++ EventHighway.Core.Tests.Unit/Services/Foundations/ListenerEvents/V2/ListenerEventV2ServiceTests.Logic.Modify.cs
@@ -0,0 +1,69 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using EventHighway.Core.Models.ListenerEvents.V2;
+using FluentAssertions;
+using Force.DeepCloner;
+using Moq;
+
+namespace EventHighway.Core.Tests.Unit.Services.Foundations.ListenerEvents.V2
+{
+    public partial class ListenerEventV2ServiceTests
+    {
+        [Fact]
+        public async Task ShouldModifyListenerEventV2Async()
+        {
+            // given
+            DateTimeOffset randomDateTimeOffset =
+                GetRandomDateTimeOffset();
+
+            ListenerEventV2 randomListenerEventV2 =
+                CreateRandomListenerEventV2(
+                    randomDateTimeOffset);
+
+            ListenerEventV2 inputListenerEventV2 =
+                randomListenerEventV2;
+
+            ListenerEventV2 storageListenerEventV2 =
+                inputListenerEventV2;
+
+            ListenerEventV2 expectedListenerEventV2 =
+                storageListenerEventV2.DeepClone();
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetDateTimeOffsetAsync())
+                    .ReturnsAsync(randomDateTimeOffset);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.UpdateListenerEventV2Async(
+                    inputListenerEventV2))
+                        .ReturnsAsync(storageListenerEventV2);
+
+            // when
+            ListenerEventV2 actualListenerEventV2 =
+                await this.listenerEventV2Service
+                    .ModifyListenerEventV2Async(
+                        inputListenerEventV2);
+
+            // then
+            actualListenerEventV2.Should().BeEquivalentTo(
+                expectedListenerEventV2);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetDateTimeOffsetAsync(),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateListenerEventV2Async(
+                    inputListenerEventV2),
+                        Times.Once);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
