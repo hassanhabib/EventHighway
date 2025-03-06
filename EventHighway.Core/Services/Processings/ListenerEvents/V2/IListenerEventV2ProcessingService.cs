ecba38bbbda9249ca6ace85e2765837c19366465
ZafarUrakov <zafarurakov@outlook.com>
2025-03-06 16:41:09 +0800

    ShouldAddListenerEventV2Async -> FAIL

---
+++ EventHighway.Core/Services/Processings/ListenerEvents/V2/IListenerEventV2ProcessingService.cs
@@ -0,0 +1,14 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using System.Threading.Tasks;
+using EventHighway.Core.Models.ListenerEvents.V2;
+
+namespace EventHighway.Core.Services.Processings.ListenerEvents.V2
+{
+    internal interface IListenerEventV2ProcessingService
+    {
+        ValueTask<ListenerEventV2> AddListenerEventV2Async(ListenerEventV2 listenerEventV2);
+    }
+}
