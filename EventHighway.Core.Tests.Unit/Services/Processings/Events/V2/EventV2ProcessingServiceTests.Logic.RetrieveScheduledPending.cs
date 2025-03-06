b721de7f178bb798be0ea7da599fdf9da857ea61
ZafarUrakov <zafarurakov@outlook.com>
2025-03-06 13:43:34 +0800

    ShouldRetrieveScheduledPendingEventV2sAsync  -> FAIL

---
+++ EventHighway.Core.Tests.Unit/Services/Processings/Events/V2/EventV2ProcessingServiceTests.Logic.RetrieveScheduledPending.cs
@@ -0,0 +1,65 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using EventHighway.Core.Models.Events.V2;
+using FluentAssertions;
+using Force.DeepCloner;
+using Moq;
+
+namespace EventHighway.Core.Tests.Unit.Services.Processings.Events.V2
+{
+    public partial class EventV2ProcessingServiceTests
+    {
+        [Fact]
+        public async Task ShouldRetrieveScheduledPendingEventV2sAsync()
+        {
+            // given
+            DateTimeOffset randomDateTimeOffset =
+                GetRandomDateTimeOffset();
+
+            DateTimeOffset retrievedDateTimeOffset =
+                randomDateTimeOffset;
+
+            IQueryable<EventV2> randomEventV2s = CreateRandomEventV2s();
+            IQueryable<EventV2> retrievedEventV2s = randomEventV2s;
+
+            IQueryable<EventV2> expectedEventV2s =
+                retrievedEventV2s.Where(eventV2 =>
+                    eventV2.Type == EventV2Type.Scheduled &&
+                    eventV2.ScheduledDate > retrievedDateTimeOffset)
+                        .DeepClone();
+
+            this.eventV2ServiceMock.Setup(service =>
+                service.RetrieveAllEventV2sAsync())
+                    .ReturnsAsync(retrievedEventV2s);
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetDateTimeOffsetAsync())
+                    .ReturnsAsync(retrievedDateTimeOffset);
+
+            // when
+            IQueryable<EventV2> actualEventV2s =
+                await this.eventV2ProcessingService
+                    .RetrieveScheduledPendingEventV2sAsync();
+
+            // then
+            actualEventV2s.Should().BeEquivalentTo(expectedEventV2s);
+
+            this.eventV2ServiceMock.Verify(service =>
+                service.RetrieveAllEventV2sAsync(),
+                    Times.Once);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetDateTimeOffsetAsync(),
+                    Times.Once);
+
+            this.eventV2ServiceMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ EventHighway.Core.Tests.Unit/Services/Processings/Events/V2/EventV2ProcessingServiceTests.cs
@@ -0,0 +1,67 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Linq;
+using EventHighway.Core.Brokers.Loggings;
+using EventHighway.Core.Brokers.Times;
+using EventHighway.Core.Models.Events.V2;
+using EventHighway.Core.Services.Foundations.Events.V2;
+using EventHighway.Core.Services.Processings.Events.V2;
+using Moq;
+using Tynamix.ObjectFiller;
+
+namespace EventHighway.Core.Tests.Unit.Services.Processings.Events.V2
+{
+    public partial class EventV2ProcessingServiceTests
+    {
+        private readonly Mock<IEventV2Service> eventV2ServiceMock;
+        private readonly Mock<IDateTimeBroker> dateTimeBrokerMock;
+        private readonly Mock<ILoggingBroker> loggingBrokerMock;
+        private readonly IEventV2ProcessingService eventV2ProcessingService;
+
+        public EventV2ProcessingServiceTests()
+        {
+            this.eventV2ServiceMock = new Mock<IEventV2Service>();
+            this.dateTimeBrokerMock = new Mock<IDateTimeBroker>();
+            this.loggingBrokerMock = new Mock<ILoggingBroker>();
+
+            this.eventV2ProcessingService =
+                new EventV2ProcessingService(
+                    eventV2Service: this.eventV2ServiceMock.Object,
+                    dateTimeBroker: this.dateTimeBrokerMock.Object,
+                    loggingBroker: this.loggingBrokerMock.Object);
+        }
+
+        private static int GetRandomNumber() =>
+            new IntRange(min: 2, max: 9).GetValue();
+
+        private static IQueryable<EventV2> CreateRandomEventV2s()
+        {
+            return CreateEventV2Filler(dates: GetRandomDateTimeOffset())
+                .Create(count: GetRandomNumber())
+                    .AsQueryable();
+        }
+
+        private static DateTimeOffset GetRandomDateTimeOffset()
+        {
+            return new DateTimeRange(
+                earliestDate: DateTime.UnixEpoch)
+                    .GetValue();
+        }
+
+        private static Filler<EventV2> CreateEventV2Filler(DateTimeOffset dates)
+        {
+            var filler = new Filler<EventV2>();
+
+            filler.Setup()
+                .OnType<DateTimeOffset>().Use(dates)
+
+                .OnType<DateTimeOffset?>()
+                    .Use(GetRandomDateTimeOffset());
+
+            return filler;
+        }
+    }
+}
