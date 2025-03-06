360d2827231b009aa75acb28879c481322ac9837
ZafarUrakov <zafarurakov@outlook.com>
2025-03-06 14:49:03 +0800

    ShouldThrowValidationOnRetrieveByEventAddressIdIfEventAddressIdIsInvalidAndLogItAsync -> PASS

---
+++ EventHighway.Core/Services/Processings/EventListeners/V2/EventListenerV2ProcessingService.Exceptions.cs
@@ -0,0 +1,71 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using System.Linq;
+using System.Threading.Tasks;
+using EventHighway.Core.Models.EventListeners.V2;
+using EventHighway.Core.Models.Processings.EventListeners.V2.Exceptions;
+using Xeptions;
+
+namespace EventHighway.Core.Services.Processings.EventListeners.V2
+{
+    internal partial class EventListenerV2ProcessingService
+    {
+        private delegate ValueTask<IQueryable<EventListenerV2>> ReturningEventListenerV2sFunction();
+
+        private async ValueTask<IQueryable<EventListenerV2>> TryCatch(
+            ReturningEventListenerV2sFunction returningEventListenerV2sFunction)
+        {
+            try
+            {
+                return await returningEventListenerV2sFunction();
+            }
+            catch (InvalidEventListenerV2ProcessingException
+                invalidEventListenerV2ProcessingException)
+            {
+                throw await CreateAndLogValidationExceptionAsync(
+                    invalidEventListenerV2ProcessingException);
+            }
+        }
+
+        private async ValueTask<EventListenerV2ProcessingValidationException> CreateAndLogValidationExceptionAsync(
+            Xeption exception)
+        {
+            var eventListenerV2ProcessingValidationException =
+                new EventListenerV2ProcessingValidationException(
+                    message: "Event listener validation error occurred, fix the errors and try again.",
+                    innerException: exception);
+
+            await this.loggingBroker.LogErrorAsync(eventListenerV2ProcessingValidationException);
+
+            return eventListenerV2ProcessingValidationException;
+        }
+
+        private async ValueTask<EventListenerV2ProcessingDependencyException> CreateAndLogDependencyExceptionAsync(
+            Xeption exception)
+        {
+            var eventListenerV2ProcessingDependencyException =
+                new EventListenerV2ProcessingDependencyException(
+                    message: "Event listener dependency error occurred, contact support.",
+                    innerException: exception.InnerException as Xeption);
+
+            await this.loggingBroker.LogErrorAsync(eventListenerV2ProcessingDependencyException);
+
+            return eventListenerV2ProcessingDependencyException;
+        }
+
+        private async ValueTask<EventListenerV2ProcessingServiceException> CreateAndLogServiceExceptionAsync(
+            Xeption exception)
+        {
+            var eventListenerV2ProcessingServiceException =
+                new EventListenerV2ProcessingServiceException(
+                    message: "Event listener service error occurred, contact support.",
+                    innerException: exception);
+
+            await this.loggingBroker.LogErrorAsync(eventListenerV2ProcessingServiceException);
+
+            return eventListenerV2ProcessingServiceException;
+        }
+    }
+}
--- EventHighway.Core/Services/Processings/EventListeners/V2/EventListenerV2ProcessingService.cs
+++ EventHighway.Core/Services/Processings/EventListeners/V2/EventListenerV2ProcessingService.cs
@@ -5,26 +5,35 @@
 using System;
 using System.Linq;
 using System.Threading.Tasks;
+using EventHighway.Core.Brokers.Loggings;
 using EventHighway.Core.Models.EventListeners.V2;
 using EventHighway.Core.Services.Foundations.EventListeners.V2;
 
 namespace EventHighway.Core.Services.Processings.EventListeners.V2
 {
-    internal class EventListenerV2ProcessingService : IEventListenerV2ProcessingService
+    internal partial class EventListenerV2ProcessingService : IEventListenerV2ProcessingService
     {
         private readonly IEventListenerV2Service eventListenerV2Service;
+        private readonly ILoggingBroker loggingBroker;
 
-        public EventListenerV2ProcessingService(IEventListenerV2Service eventListenerV2Service) =>
-            this.eventListenerV2Service = eventListenerV2Service;
-
-        public async ValueTask<IQueryable<EventListenerV2>> RetrieveEventListenerV2sByEventAddressIdAsync(
-            Guid eventAddressId)
+        public EventListenerV2ProcessingService(
+            IEventListenerV2Service eventListenerV2Service,
+            ILoggingBroker loggingBroker)
         {
+            this.eventListenerV2Service = eventListenerV2Service;
+            this.loggingBroker = loggingBroker;
+        }
+
+        public ValueTask<IQueryable<EventListenerV2>> RetrieveEventListenerV2sByEventAddressIdAsync(
+            Guid eventAddressId) => TryCatch(async () =>
+        {
+            ValidateEventAddressId(eventAddressId);
+
             IQueryable<EventListenerV2> eventListenerV2s =
                 await this.eventListenerV2Service.RetrieveAllEventListenerV2sAsync();
 
             return eventListenerV2s.Where(eventListenerV2 =>
                 eventListenerV2.EventAddressId == eventAddressId);
-        }
+        });
     }
 }
