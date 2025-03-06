d12393c4620e55ca488f39ed105e54b9ff451196
ZafarUrakov <zafarurakov@outlook.com>
2025-03-06 16:41:15 +0800

    ShouldAddListenerEventV2Async -> PASS

---
+++ EventHighway.Core/Services/Processings/ListenerEvents/V2/ListenerEventV2ProcessingService.cs
@@ -0,0 +1,28 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using System.Threading.Tasks;
+using EventHighway.Core.Brokers.Loggings;
+using EventHighway.Core.Models.ListenerEvents.V2;
+using EventHighway.Core.Services.Foundations.ListernEvents.V2;
+
+namespace EventHighway.Core.Services.Processings.ListenerEvents.V2
+{
+    internal partial class ListenerEventV2ProcessingService : IListenerEventV2ProcessingService
+    {
+        private readonly IListenerEventV2Service listenerEventV2Service;
+        private readonly ILoggingBroker loggingBroker;
+
+        public ListenerEventV2ProcessingService(
+            IListenerEventV2Service listenerEventV2Service,
+            ILoggingBroker loggingBroker)
+        {
+            this.listenerEventV2Service = listenerEventV2Service;
+            this.loggingBroker = loggingBroker;
+        }
+
+        public async ValueTask<ListenerEventV2> AddListenerEventV2Async(ListenerEventV2 listenerEventV2) =>
+            await this.listenerEventV2Service.AddListenerEventV2Async(listenerEventV2);
+    }
+}
