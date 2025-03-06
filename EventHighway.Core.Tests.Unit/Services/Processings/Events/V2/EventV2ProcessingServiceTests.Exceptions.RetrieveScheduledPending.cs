97c7355eb4deb5a9bb6ae7c861d8b585d8b8b475
ZafarUrakov <zafarurakov@outlook.com>
2025-03-06 13:54:24 +0800

    ShouldThrowServiceExceptionOnRetrieveScheduledPendingIfExceptionOccursAndLogItAsync -> FAIL

--- EventHighway.Core.Tests.Unit/Services/Processings/Events/V2/EventV2ProcessingServiceTests.Exceptions.RetrieveScheduledPending.cs
+++ EventHighway.Core.Tests.Unit/Services/Processings/Events/V2/EventV2ProcessingServiceTests.Exceptions.RetrieveScheduledPending.cs
@@ -2,6 +2,7 @@
 // Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
 // ----------------------------------------------------------------------------------
 
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 using EventHighway.Core.Models.Events.V2;
@@ -48,6 +49,56 @@ namespace EventHighway.Core.Tests.Unit.Services.Processings.Events.V2
             this.loggingBrokerMock.Verify(broker =>
                 broker.LogErrorAsync(It.Is(SameExceptionAs(
                     expectedEventV2ProcessingDependencyException))),
+                        Times.Once);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetDateTimeOffsetAsync(),
+                    Times.Never);
+
+            this.eventV2ServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowServiceExceptionOnRetrieveScheduledPendingIfExceptionOccursAndLogItAsync()
+        {
+            // given
+            var serviceException = new Exception();
+
+            var failedEventV2ProcessingServiceException =
+                new FailedEventV2ProcessingServiceException(
+                    message: "Failed event service error occurred, contact support.",
+                    innerException: serviceException);
+
+            var expectedEventV2ProcessingServiceException =
+                new EventV2ProcessingServiceException(
+                    message: "Event service error occurred, contact support.",
+                    innerException: failedEventV2ProcessingServiceException);
+
+            this.eventV2ServiceMock.Setup(service =>
+                service.RetrieveAllEventV2sAsync())
+                    .ThrowsAsync(serviceException);
+
+            // when
+            ValueTask<IQueryable<EventV2>> retrieveScheduledPendingEventV2sTask =
+                this.eventV2ProcessingService.RetrieveScheduledPendingEventV2sAsync();
+
+            EventV2ProcessingServiceException actualEventV2ProcessingServiceException =
+                await Assert.ThrowsAsync<EventV2ProcessingServiceException>(
+                    retrieveScheduledPendingEventV2sTask.AsTask);
+
+            // then
+            actualEventV2ProcessingServiceException.Should()
+                .BeEquivalentTo(expectedEventV2ProcessingServiceException);
+
+            this.eventV2ServiceMock.Verify(service =>
+                service.RetrieveAllEventV2sAsync(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedEventV2ProcessingServiceException))),
                         Times.Once);
 
             this.dateTimeBrokerMock.Verify(broker =>
