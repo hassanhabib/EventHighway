062712e578ae94bb94a0b156e41734e305427ab0
ZafarUrakov <zafarurakov@outlook.com>
2025-03-07 14:34:44 +0800

    ShouldThrowValidationExceptionOnRunIfEventCallV2IsNullAndLogItAsync -> FAIL

---
+++ EventHighway.Core/Models/Orchestrations/Events/V2/Exceptions/EventV2OrchestrationDependencyValidationException.cs
@@ -0,0 +1,15 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using Xeptions;
+
+namespace EventHighway.Core.Models.Orchestrations.Events.V2.Exceptions
+{
+    public class EventV2OrchestrationDependencyValidationException : Xeption
+    {
+        public EventV2OrchestrationDependencyValidationException(string message, Xeption innerException)
+            : base(message, innerException)
+        { }
+    }
+}
