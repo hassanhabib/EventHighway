ee68f8fcfebe77d61b49012f4d6671db7646e6ab
ZafarUrakov <zafarurakov@outlook.com>
2025-03-07 17:02:28 +0800

    ShouldAddListenerEventV2Async -> PASS

--- EventHighway.Core/Services/Orchestrations/EventListeners/V2/IEventListenerV2OrchestrationService.cs
+++ EventHighway.Core/Services/Orchestrations/EventListeners/V2/IEventListenerV2OrchestrationService.cs
@@ -6,11 +6,13 @@ using System;
 using System.Linq;
 using System.Threading.Tasks;
 using EventHighway.Core.Models.EventListeners.V2;
+using EventHighway.Core.Models.ListenerEvents.V2;
 
 namespace EventHighway.Core.Services.Orchestrations.EventListeners.V2
 {
     internal interface IEventListenerV2OrchestrationService
     {
         ValueTask<IQueryable<EventListenerV2>> RetrieveEventListenerV2sByEventAddressIdAsync(Guid eventAddressId);
+        ValueTask<ListenerEventV2> AddListenerEventV2Async(ListenerEventV2 listenerEventV2);
     }
 }
