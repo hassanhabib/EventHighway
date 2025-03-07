74e79c1442c657fc6ba1af413a9bbb6f9af011f7
ZafarUrakov <zafarurakov@outlook.com>
2025-03-07 17:26:52 +0800

    ShouldThrowDependencyValidationOnModifyListenerEventV2IfDependencyValidationAndLogItAsync -> PASS

---
+++ EventHighway.Core.Tests.Unit/Services/Orchestrations/EventListeners/V2/EventListenerV2OrchestrationServiceTests.Exceptions.ModifyListenerEventV2.cs
@@ -0,0 +1,60 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using System.Threading.Tasks;
+using EventHighway.Core.Models.ListenerEvents.V2;
+using EventHighway.Core.Models.Orchestrations.EventListeners.V2.Exceptions;
+using FluentAssertions;
+using Moq;
+using Xeptions;
+
+namespace EventHighway.Core.Tests.Unit.Services.Orchestrations.EventListeners.V2
+{
+    public partial class EventListenerV2OrchestrationServiceTests
+    {
+        [Theory]
+        [MemberData(nameof(ListenerEventV2ValidationExceptions))]
+        public async Task ShouldThrowDependencyValidationOnModifyListenerEventV2IfDependencyValidationAndLogItAsync(
+            Xeption listenerEventV2ValidationException)
+        {
+            // given
+            ListenerEventV2 someListenerEventV2 = CreateRandomListenerEventV2();
+
+            var expectedEventListenerV2OrchestrationDependencyValidationException =
+                new EventListenerV2OrchestrationDependencyValidationException(
+                    message: "Event listener validation error occurred, fix the errors and try again.",
+                    innerException: listenerEventV2ValidationException.InnerException as Xeption);
+
+            this.listenerEventV2ProcessingServiceMock.Setup(service =>
+                service.ModifyListenerEventV2Async(It.IsAny<ListenerEventV2>()))
+                    .ThrowsAsync(listenerEventV2ValidationException);
+
+            // when
+            ValueTask<ListenerEventV2> modifyListenerEventV2Task =
+                this.eventListenerV2OrchestrationService.ModifyListenerEventV2Async(someListenerEventV2);
+
+            EventListenerV2OrchestrationDependencyValidationException
+                actualEventListenerV2OrchestrationDependencyValidationException =
+                    await Assert.ThrowsAsync<EventListenerV2OrchestrationDependencyValidationException>(
+                        modifyListenerEventV2Task.AsTask);
+
+            // then
+            actualEventListenerV2OrchestrationDependencyValidationException.Should()
+                .BeEquivalentTo(expectedEventListenerV2OrchestrationDependencyValidationException);
+
+            this.listenerEventV2ProcessingServiceMock.Verify(service =>
+                service.ModifyListenerEventV2Async(It.IsAny<ListenerEventV2>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedEventListenerV2OrchestrationDependencyValidationException))),
+                        Times.Once);
+
+            this.listenerEventV2ProcessingServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.eventListenerV2ProcessingServiceMock.VerifyNoOtherCalls();
+        }
+    }
+}
