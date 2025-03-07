6b000a4076706524741f3fe96ec03ed06a725bb9
ZafarUrakov <zafarurakov@outlook.com>
2025-03-07 17:02:24 +0800

    ShouldAddListenerEventV2Async -> FAIL

--- EventHighway.Core.Tests.Unit/Services/Orchestrations/EventListeners/V2/EventListenerV2OrchestrationServiceTests.cs
+++ EventHighway.Core.Tests.Unit/Services/Orchestrations/EventListeners/V2/EventListenerV2OrchestrationServiceTests.cs
@@ -7,10 +7,11 @@ using System.Linq;
 using System.Linq.Expressions;
 using EventHighway.Core.Brokers.Loggings;
 using EventHighway.Core.Models.EventListeners.V2;
+using EventHighway.Core.Models.ListenerEvents.V2;
 using EventHighway.Core.Models.Processings.EventListeners.V2.Exceptions;
-using EventHighway.Core.Models.Processings.Events.V2.Exceptions;
 using EventHighway.Core.Services.Orchestrations.EventListeners.V2;
 using EventHighway.Core.Services.Processings.EventListeners.V2;
+using EventHighway.Core.Services.Processings.ListenerEvents.V2;
 using Moq;
 using Tynamix.ObjectFiller;
 using Xeptions;
@@ -20,6 +21,7 @@ namespace EventHighway.Core.Tests.Unit.Services.Orchestrations.EventListeners.V2
     public partial class EventListenerV2OrchestrationServiceTests
     {
         private readonly Mock<IEventListenerV2ProcessingService> eventListenerV2ProcessingServiceMock;
+        private readonly Mock<IListenerEventV2ProcessingService> listenerEventV2ProcessingServiceMock;
         private readonly Mock<ILoggingBroker> loggingBrokerMock;
         private readonly IEventListenerV2OrchestrationService eventListenerV2OrchestrationService;
 
@@ -28,14 +30,18 @@ namespace EventHighway.Core.Tests.Unit.Services.Orchestrations.EventListeners.V2
             this.eventListenerV2ProcessingServiceMock =
                 new Mock<IEventListenerV2ProcessingService>();
 
+            this.listenerEventV2ProcessingServiceMock =
+                new Mock<IListenerEventV2ProcessingService>();
+
             this.loggingBrokerMock = new Mock<ILoggingBroker>();
 
             this.eventListenerV2OrchestrationService =
                 new EventListenerV2OrchestrationService(
                     eventListenerV2ProcessingService: this.eventListenerV2ProcessingServiceMock.Object,
+                    listenerEventV2ProcessingService: this.listenerEventV2ProcessingServiceMock.Object,
                     loggingBroker: this.loggingBrokerMock.Object);
         }
-        
+
         public static TheoryData<Xeption> EventListenerV2DependencyExceptions()
         {
             string someMessage = GetRandomString();
@@ -68,8 +74,30 @@ namespace EventHighway.Core.Tests.Unit.Services.Orchestrations.EventListeners.V2
         private static DateTimeOffset GetRandomDateTimeOffset() =>
             new DateTimeRange(earliestDate: DateTime.UnixEpoch).GetValue();
 
+        private static ListenerEventV2 CreateRandomListenerEventV2() =>
+            CreateListenerEventV2Filler().Create();
+
         private static IQueryable<EventListenerV2> CreateRandomEventListenerV2s() =>
             CreateEventListenerV2Filler().Create(count: GetRandomNumber()).AsQueryable();
+
+        private static Filler<ListenerEventV2> CreateListenerEventV2Filler()
+        {
+            var filler = new Filler<ListenerEventV2>();
+
+            filler.Setup()
+                .OnType<DateTimeOffset>().Use(GetRandomDateTimeOffset)
+
+                .OnProperty(listenerEventV2 => listenerEventV2.Event)
+                    .IgnoreIt()
+
+                .OnProperty(listenerEventV2 => listenerEventV2.EventAddress)
+                    .IgnoreIt()
+
+                .OnProperty(listenerEventV2 => listenerEventV2.EventListener)
+                    .IgnoreIt();
+
+            return filler;
+        }
 
         private static Filler<EventListenerV2> CreateEventListenerV2Filler()
         {
