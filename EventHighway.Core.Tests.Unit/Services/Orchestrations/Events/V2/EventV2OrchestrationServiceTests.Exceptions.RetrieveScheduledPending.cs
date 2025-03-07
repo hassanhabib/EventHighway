ccd54438475cb0e31d4b5eb5588510096b1bba73
ZafarUrakov <zafarurakov@outlook.com>
2025-03-07 12:03:23 +0800

    ShouldThrowDependencyExceptionOnRetrieveScheduledPendingIfEventV2DependencyAndLogItAsync -> FAIL

---
+++ EventHighway.Core.Tests.Unit/Services/Orchestrations/Events/V2/EventV2OrchestrationServiceTests.Exceptions.RetrieveScheduledPending.cs
@@ -0,0 +1,57 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using EventHighway.Core.Models.Events.V2;
+using EventHighway.Core.Models.Orchestrations.Events.V2.Exceptions;
+using FluentAssertions;
+using Moq;
+using System.Linq;
+using System.Threading.Tasks;
+using Xeptions;
+
+namespace EventHighway.Core.Tests.Unit.Services.Orchestrations.Events.V2
+{
+    public partial class EventV2OrchestrationServiceTests
+    {
+        [Theory]
+        [MemberData(nameof(EventV2DependencyExceptions))]
+        public async Task ShouldThrowDependencyExceptionOnRetrieveScheduledPendingIfEventV2DependencyAndLogItAsync(
+            Xeption eventV2DependencyException)
+        {
+            // given
+            var expectedEventV2OrchestrationDependencyException =
+                new EventV2OrchestrationDependencyException(
+                    message: "Event dependency error occurred, contact support.",
+                    innerException: eventV2DependencyException.InnerException as Xeption);
+
+            this.eventV2ProcessingServiceMock.Setup(service =>
+                service.RetrieveScheduledPendingEventV2sAsync())
+                    .ThrowsAsync(eventV2DependencyException);
+
+            // when
+            ValueTask<IQueryable<EventV2>> retrieveScheduledPendingEventV2sTask =
+                this.eventV2OrchestrationService.RetrieveScheduledPendingEventV2sAsync();
+
+            EventV2OrchestrationDependencyException actualEventV2OrchestrationDependencyException =
+                await Assert.ThrowsAsync<EventV2OrchestrationDependencyException>(
+                    retrieveScheduledPendingEventV2sTask.AsTask);
+
+            // then
+            actualEventV2OrchestrationDependencyException.Should()
+                .BeEquivalentTo(expectedEventV2OrchestrationDependencyException);
+
+            this.eventV2ProcessingServiceMock.Verify(service =>
+                service.RetrieveScheduledPendingEventV2sAsync(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedEventV2OrchestrationDependencyException))),
+                        Times.Once);
+
+            this.eventV2ProcessingServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
--- EventHighway.Core.Tests.Unit/Services/Orchestrations/Events/V2/EventV2OrchestrationServiceTests.cs
+++ EventHighway.Core.Tests.Unit/Services/Orchestrations/Events/V2/EventV2OrchestrationServiceTests.cs
@@ -4,12 +4,15 @@
 
 using System;
 using System.Linq;
+using System.Linq.Expressions;
 using EventHighway.Core.Brokers.Loggings;
 using EventHighway.Core.Models.Events.V2;
+using EventHighway.Core.Models.Processings.Events.V2.Exceptions;
 using EventHighway.Core.Services.Orchestrations.Events.V2;
 using EventHighway.Core.Services.Processings.Events.V2;
 using Moq;
 using Tynamix.ObjectFiller;
+using Xeptions;
 
 namespace EventHighway.Core.Tests.Unit.Services.Orchestrations.Events.V2
 {
@@ -32,6 +35,29 @@ namespace EventHighway.Core.Tests.Unit.Services.Orchestrations.Events.V2
                     eventV2ProcessingService: this.eventV2ProcessingServiceMock.Object,
                     loggingBroker: loggingBrokerMock.Object);
         }
+
+        public static TheoryData<Xeption> EventV2DependencyExceptions()
+        {
+            string someMessage = GetRandomString();
+            var someInnerException = new Xeption();
+
+            return new TheoryData<Xeption>
+            {
+                new EventV2ProcessingDependencyException(
+                    someMessage,
+                    someInnerException),
+
+                new EventV2ProcessingServiceException(
+                    someMessage,
+                    someInnerException),
+            };
+        }
+
+        private static Expression<Func<Xeption, bool>> SameExceptionAs(Xeption expectedException) =>
+            actualException => actualException.SameExceptionAs(expectedException);
+
+        private static string GetRandomString() =>
+            new MnemonicString().GetValue();
 
         private static int GetRandomNumber() =>
             new IntRange(min: 2, max: 9).GetValue();
