9ce359aa38add8f083a32988b4edac89df65f8d1
ZafarUrakov <zafarurakov@outlook.com>
2025-03-11 07:37:25 +0800

    ShouldFireScheduledPendingEventV2sAsync -> PASS

---
+++ EventHighway.Core/Clients/Events/V2/EventV2sClient.cs
@@ -0,0 +1,23 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using System.Threading.Tasks;
+using EventHighway.Core.Services.Coordinations.Events.V2;
+
+namespace EventHighway.Core.Clients.Events.V2
+{
+    internal class EventV2sClient : IEventV2sClient
+    {
+        private readonly IEventV2CoordinationService eventV2CoordinationService;
+
+        public EventV2sClient(IEventV2CoordinationService eventV2CoordinationService) =>
+            this.eventV2CoordinationService = eventV2CoordinationService;
+
+        public async ValueTask FireScheduledPendingEventV2sAsync()
+        {
+            await this.eventV2CoordinationService
+                .FireScheduledPendingEventV2sAsync();
+        }
+    }
+}
