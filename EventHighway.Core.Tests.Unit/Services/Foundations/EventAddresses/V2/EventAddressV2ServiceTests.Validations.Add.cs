90afdd6e2196c663985b07d984b5383d80a1bcc5
ZafarUrakov <zafarurakov@outlook.com>
2025-03-13 08:34:40 +0800

    ShouldThrowValidationExceptionOnAddIfEventAddressV2IsInvalidAndLogItAsync -> FAIL

--- EventHighway.Core.Tests.Unit/Services/Foundations/EventAddresses/V2/EventAddressV2ServiceTests.Validations.Add.cs
+++ EventHighway.Core.Tests.Unit/Services/Foundations/EventAddresses/V2/EventAddressV2ServiceTests.Validations.Add.cs
@@ -2,6 +2,7 @@
 // Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
 // ----------------------------------------------------------------------------------
 
+using System;
 using System.Threading.Tasks;
 using EventHighway.Core.Models.Services.Foundations.EventAddresses.V2;
 using EventHighway.Core.Models.Services.Foundations.EventAddresses.V2.Exceptions;
@@ -53,6 +54,79 @@ namespace EventHighway.Core.Tests.Unit.Services.Foundations.EventAddresses.V2
 
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        private async Task ShouldThrowValidationExceptionOnAddIfEventAddressV2IsInvalidAndLogItAsync(
+            string invalidText)
+        {
+            var invalidEventAddressV2 = new EventAddressV2
+            {
+                Id = Guid.Empty,
+                Name = invalidText,
+                Description = invalidText,
+            };
+
+            var invalidEventAddressV2Exception =
+                new InvalidEventAddressV2Exception(
+                    message: "Event address is invalid, fix the errors and try again.");
+
+            invalidEventAddressV2Exception.AddData(
+                key: nameof(EventAddressV2.Id),
+                values: "Required");
+
+            invalidEventAddressV2Exception.AddData(
+                key: nameof(EventAddressV2.Name),
+                values: "Required");
+
+            invalidEventAddressV2Exception.AddData(
+                key: nameof(EventAddressV2.Description),
+                values: "Required");
+
+            invalidEventAddressV2Exception.AddData(
+                key: nameof(EventAddressV2.CreatedDate),
+                values: "Required");
+
+            invalidEventAddressV2Exception.AddData(
+                key: nameof(EventAddressV2.UpdatedDate),
+                values: "Required");
+
+            var expectedEventAddressV2ValidationException =
+                new EventAddressV2ValidationException(
+                    message: "Event address validation error occurred, fix the errors and try again.",
+                    innerException: invalidEventAddressV2Exception);
+
+            // when
+            ValueTask<EventAddressV2> addEventAddressV2Task =
+                this.eventAddressV2Service.AddEventAddressV2Async(invalidEventAddressV2);
+
+            EventAddressV2ValidationException actualEventAddressV2ValidationException =
+                await Assert.ThrowsAsync<EventAddressV2ValidationException>(
+                    addEventAddressV2Task.AsTask);
+
+            // then
+            actualEventAddressV2ValidationException.Should().BeEquivalentTo(
+                expectedEventAddressV2ValidationException);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetDateTimeOffsetAsync(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedEventAddressV2ValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertEventAddressV2Async(It.IsAny<EventAddressV2>()),
+                    Times.Never);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
         }
     }
