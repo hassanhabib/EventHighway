e78ea4dc13465f2cb84a6911c32c98ef04bfd69e
ZafarUrakov <zafarurakov@outlook.com>
2025-03-13 14:28:29 +0800

    ShouldRemoveEventV2ByIdAsync -> PASS

--- EventHighway.Core/Services/Foundations/Events/V2/EventV2Service.cs
+++ EventHighway.Core/Services/Foundations/Events/V2/EventV2Service.cs
@@ -2,6 +2,7 @@
 // Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
 // ----------------------------------------------------------------------------------
 
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 using EventHighway.Core.Brokers.Loggings;
@@ -37,5 +38,13 @@ namespace EventHighway.Core.Services.Foundations.Events.V2
 
         public ValueTask<IQueryable<EventV2>> RetrieveAllEventV2sAsync() =>
         TryCatch(async () => await this.storageBroker.SelectAllEventV2sAsync());
+
+        public async ValueTask<EventV2> RemoveEventV2ByIdAsync(Guid eventV2Id)
+        {
+            EventV2 maybeEventV2 =
+                await this.storageBroker.SelectEventV2ByIdAsync(eventV2Id);
+
+            return await this.storageBroker.DeleteEventV2Async(maybeEventV2);
+        }
     }
 }
