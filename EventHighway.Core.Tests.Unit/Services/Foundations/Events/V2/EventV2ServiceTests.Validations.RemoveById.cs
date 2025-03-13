434c40ad2931a37b390307ae14fcf4c0758296b4
ZafarUrakov <zafarurakov@outlook.com>
2025-03-13 14:35:05 +0800

    ShouldThrowValidationExceptionOnRemoveByIdIfIdIsInvalidAndLogItAsync -> FAIL

---
+++ EventHighway.Core.Tests.Unit/Services/Foundations/Events/V2/EventV2ServiceTests.Validations.RemoveById.cs
@@ -0,0 +1,63 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using EventHighway.Core.Models.Services.Foundations.Events.V2;
+using EventHighway.Core.Models.Services.Foundations.Events.V2.Exceptions;
+using FluentAssertions;
+using Moq;
+
+namespace EventHighway.Core.Tests.Unit.Services.Foundations.Events.V2
+{
+    public partial class EventV2ServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnRemoveByIdIfIdIsInvalidAndLogItAsync()
+        {
+            // given
+            Guid invalidEventV2Id = Guid.Empty;
+
+            var invalidEventV2Exception =
+                new InvalidEventV2Exception(
+                    message: "Event is invalid, fix the errors and try again.");
+
+            invalidEventV2Exception.AddData(
+                key: nameof(EventV2.Id),
+                values: "Required");
+
+            var expectedEventV2ValidationException =
+                new EventV2ValidationException(
+                    message: "Event validation error occurred, fix the errors and try again.",
+                    innerException: invalidEventV2Exception);
+
+            // when
+            ValueTask<EventV2> removeEventV2ByIdTask =
+                this.eventV2Service.RemoveEventV2ByIdAsync(
+                    invalidEventV2Id);
+
+            EventV2ValidationException actualEventV2ValidationException =
+                await Assert.ThrowsAsync<EventV2ValidationException>(
+                    removeEventV2ByIdTask.AsTask);
+
+            // then
+            actualEventV2ValidationException.Should()
+                .BeEquivalentTo(expectedEventV2ValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedEventV2ValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectEventV2ByIdAsync(
+                    It.IsAny<Guid>()),
+                        Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
