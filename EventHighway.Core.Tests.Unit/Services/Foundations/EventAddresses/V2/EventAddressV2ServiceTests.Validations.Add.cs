cd7479d39085b74a1dcb993b95e1b2d4b3abddd3
ZafarUrakov <zafarurakov@outlook.com>
2025-03-13 08:36:33 +0800

    ShouldThrowValidationExceptionOnAddIfCreatedDateIsNotSameAsUpdatedDateAndLogItAsync -> FAIL

--- EventHighway.Core.Tests.Unit/Services/Foundations/EventAddresses/V2/EventAddressV2ServiceTests.Validations.Add.cs
+++ EventHighway.Core.Tests.Unit/Services/Foundations/EventAddresses/V2/EventAddressV2ServiceTests.Validations.Add.cs
@@ -129,5 +129,62 @@ namespace EventHighway.Core.Tests.Unit.Services.Foundations.EventAddresses.V2
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnAddIfCreatedDateIsNotSameAsUpdatedDateAndLogItAsync()
+        {
+            // given
+            DateTimeOffset randomDateTimeOffset = GetRandomDateTimeOffset();
+            DateTimeOffset anotherRandomDateTimeOffset = GetRandomDateTimeOffset();
+            EventAddressV2 randomEventAddressV2 = CreateRandomEventAddressV2(randomDateTimeOffset);
+            EventAddressV2 invalidEventAddressV2 = randomEventAddressV2;
+            invalidEventAddressV2.UpdatedDate = anotherRandomDateTimeOffset;
+
+            var invalidEventAddressV2Exception =
+                new InvalidEventAddressV2Exception(
+                    message: "Event address is invalid, fix the errors and try again.");
+
+            invalidEventAddressV2Exception.AddData(
+                key: nameof(EventAddressV2.CreatedDate),
+                values: $"Date is not the same as {nameof(EventAddressV2.UpdatedDate)}");
+
+            var expectedEventAddressV2ValidationException =
+                new EventAddressV2ValidationException(
+                    message: "Event address validation error occurred, fix the errors and try again.",
+                    innerException: invalidEventAddressV2Exception);
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetDateTimeOffsetAsync())
+                    .ReturnsAsync(randomDateTimeOffset);
+
+            // when
+            ValueTask<EventAddressV2> addEventAddressV2Task =
+                this.eventAddressV2Service.AddEventAddressV2Async(invalidEventAddressV2);
+
+            EventAddressV2ValidationException actualEventAddressV2ValidationException =
+                await Assert.ThrowsAsync<EventAddressV2ValidationException>(
+                    addEventAddressV2Task.AsTask);
+
+            // then
+            actualEventAddressV2ValidationException.Should().BeEquivalentTo(
+                expectedEventAddressV2ValidationException);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetDateTimeOffsetAsync(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedEventAddressV2ValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertEventAddressV2Async(It.IsAny<EventAddressV2>()),
+                    Times.Never);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
