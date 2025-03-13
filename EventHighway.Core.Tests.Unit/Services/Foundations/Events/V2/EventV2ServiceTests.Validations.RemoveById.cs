9eed6e1096cc3dd9021db2c79d6fdbfff66d725f
ZafarUrakov <zafarurakov@outlook.com>
2025-03-13 14:37:22 +0800

    ShouldThrowValidationExceptionOnRemoveByIdIfEventV2IsNotFoundAndLogItAsync -> FAIL

--- EventHighway.Core.Tests.Unit/Services/Foundations/Events/V2/EventV2ServiceTests.Validations.RemoveById.cs
+++ EventHighway.Core.Tests.Unit/Services/Foundations/Events/V2/EventV2ServiceTests.Validations.RemoveById.cs
@@ -4,6 +4,8 @@
 
 using System;
 using System.Threading.Tasks;
+using EventHighway.Core.Models.Services.Foundations.EventAddresses.V2.Exceptions;
+using EventHighway.Core.Models.Services.Foundations.EventAddresses.V2;
 using EventHighway.Core.Models.Services.Foundations.Events.V2;
 using EventHighway.Core.Models.Services.Foundations.Events.V2.Exceptions;
 using FluentAssertions;
@@ -58,6 +60,58 @@ namespace EventHighway.Core.Tests.Unit.Services.Foundations.Events.V2
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnRemoveByIdIfEventV2IsNotFoundAndLogItAsync()
+        {
+            // given
+            Guid nonExistingEventV2Id = GetRandomId();
+            EventV2 nullEventV2 = null;
+
+            var notFoundEventV2Exception =
+                new NotFoundEventV2Exception(
+                    message: $"Could not find event with id: {nonExistingEventV2Id}.");
+
+            var expectedEventV2ValidationException =
+                new EventV2ValidationException(
+                    message: "Event validation error occurred, fix the errors and try again.",
+                    innerException: notFoundEventV2Exception);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectEventV2ByIdAsync(It.IsAny<Guid>()))
+                    .ReturnsAsync(nullEventV2);
+
+            // when
+            ValueTask<EventV2> removeEventV2ByIdTask =
+                this.eventV2Service.RemoveEventV2ByIdAsync(nonExistingEventV2Id);
+
+            EventV2ValidationException actualEventV2ValidationException =
+                await Assert.ThrowsAsync<EventV2ValidationException>(
+                    removeEventV2ByIdTask.AsTask);
+
+            // then
+            actualEventV2ValidationException.Should()
+                .BeEquivalentTo(expectedEventV2ValidationException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectEventV2ByIdAsync(
+                    It.IsAny<Guid>()),
+                        Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedEventV2ValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.DeleteEventV2Async(
+                    It.IsAny<EventV2>()),
+                        Times.Never);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
         }
     }
 }
---
+++ EventHighway.Core/Models/Services/Foundations/Events/V2/Exceptions/NotFoundEventV2Exception.cs
@@ -0,0 +1,15 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using Xeptions;
+
+namespace EventHighway.Core.Models.Services.Foundations.Events.V2.Exceptions
+{
+    public class NotFoundEventV2Exception : Xeption
+    {
+        public NotFoundEventV2Exception(string message)
+            : base(message)
+        { }
+    }
+}
