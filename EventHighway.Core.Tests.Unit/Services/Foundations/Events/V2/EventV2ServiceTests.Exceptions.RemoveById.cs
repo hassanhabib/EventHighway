58e4f5e283f1e7c23c3f6a10d963839d4eec8c73
ZafarUrakov <zafarurakov@outlook.com>
2025-03-13 14:52:03 +0800

    ShouldThrowDependencyValidationErrorOnRemoveByIdIfDbUpdateConcurrencyErrorAndLogItAsync -> FAIL

--- EventHighway.Core.Tests.Unit/Services/Foundations/Events/V2/EventV2ServiceTests.Exceptions.RemoveById.cs
+++ EventHighway.Core.Tests.Unit/Services/Foundations/Events/V2/EventV2ServiceTests.Exceptions.RemoveById.cs
@@ -4,10 +4,13 @@
 
 using System;
 using System.Threading.Tasks;
+using EventHighway.Core.Models.Services.Foundations.EventAddresses.V2.Exceptions;
+using EventHighway.Core.Models.Services.Foundations.EventAddresses.V2;
 using EventHighway.Core.Models.Services.Foundations.Events.V2;
 using EventHighway.Core.Models.Services.Foundations.Events.V2.Exceptions;
 using FluentAssertions;
 using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 using Moq;
 
 namespace EventHighway.Core.Tests.Unit.Services.Foundations.Events.V2
@@ -55,6 +58,55 @@ namespace EventHighway.Core.Tests.Unit.Services.Foundations.Events.V2
             this.loggingBrokerMock.Verify(broker =>
                 broker.LogCriticalAsync(It.Is(SameExceptionAs(
                     expectedEventV2DependencyException))),
+                        Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowDependencyValidationErrorOnRemoveByIdIfDbUpdateConcurrencyErrorAndLogItAsync()
+        {
+            // given
+            Guid someEventV2Id = GetRandomId();
+            var dbUpdateConcurrencyException = new DbUpdateConcurrencyException();
+
+            var lockedEventV2Exception =
+                new LockedEventV2Exception(
+                    message: "Event is locked, try again.",
+                    innerException: dbUpdateConcurrencyException);
+
+            var expectedEventV2DependencyValidationException =
+                new EventV2DependencyValidationException(
+                    message: "Event validation error occurred, fix the errors and try again.",
+                    innerException: lockedEventV2Exception);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectEventV2ByIdAsync(It.IsAny<Guid>()))
+                    .ThrowsAsync(dbUpdateConcurrencyException);
+
+            // when
+            ValueTask<EventV2> removeEventV2ByIdTask =
+                this.eventV2Service.RemoveEventV2ByIdAsync(
+                    someEventV2Id);
+
+            EventV2DependencyValidationException actualEventV2DependencyValidationException =
+                await Assert.ThrowsAsync<EventV2DependencyValidationException>(
+                    removeEventV2ByIdTask.AsTask);
+
+            // then
+            actualEventV2DependencyValidationException.Should()
+                .BeEquivalentTo(expectedEventV2DependencyValidationException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectEventV2ByIdAsync(
+                    It.IsAny<Guid>()),
+                        Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedEventV2DependencyValidationException))),
                         Times.Once);
 
             this.storageBrokerMock.VerifyNoOtherCalls();
