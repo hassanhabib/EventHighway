9e0f39cb677ca1d078c7faaa3ffee180a632d0bb
ZafarUrakov <zafarurakov@outlook.com>
2025-03-13 17:12:10 +0800

    BROKERS: Select Event Listener V2 By Id

--- EventHighway.Core/Brokers/Storages/IStorageBroker.EventListenerV2s.cs
+++ EventHighway.Core/Brokers/Storages/IStorageBroker.EventListenerV2s.cs
@@ -2,6 +2,7 @@
 // Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
 // ----------------------------------------------------------------------------------
 
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 using EventHighway.Core.Models.Services.Foundations.EventListeners.V2;
@@ -12,6 +13,7 @@ namespace EventHighway.Core.Brokers.Storages
     {
         ValueTask<EventListenerV2> InsertEventListenerV2Async(EventListenerV2 eventListenerV2);
         ValueTask<IQueryable<EventListenerV2>> SelectAllEventListenerV2sAsync();
+        ValueTask<EventListenerV2> SelectEventListenerV2ByIdAsync(Guid eventListenerV2Id);
         ValueTask<EventListenerV2> DeleteEventListenerV2Async(EventListenerV2 eventListenerV2);
     }
 }
