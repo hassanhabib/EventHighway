e59dd1baef0b3665d97729b33eecc09a6ecd62d2
ZafarUrakov <zafarurakov@outlook.com>
2025-03-13 08:46:05 +0800

    ShouldThrowDependencyExceptionOnAddIfDbUpdateExceptionOccursAndLogItAsync -> FAIL

--- EventHighway.Core.Tests.Unit/Services/Foundations/EventAddresses/V2/EventAddressV2ServiceTests.Exceptions.Add.cs
+++ EventHighway.Core.Tests.Unit/Services/Foundations/EventAddresses/V2/EventAddressV2ServiceTests.Exceptions.Add.cs
@@ -6,10 +6,9 @@ using System.Threading.Tasks;
 using EFxceptions.Models.Exceptions;
 using EventHighway.Core.Models.Services.Foundations.EventAddresses.V2;
 using EventHighway.Core.Models.Services.Foundations.EventAddresses.V2.Exceptions;
-using EventHighway.Core.Models.Services.Foundations.Events.V2.Exceptions;
-using EventHighway.Core.Models.Services.Foundations.Events.V2;
 using FluentAssertions;
 using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 using Moq;
 
 namespace EventHighway.Core.Tests.Unit.Services.Foundations.EventAddresses.V2
@@ -108,6 +107,57 @@ namespace EventHighway.Core.Tests.Unit.Services.Foundations.EventAddresses.V2
             this.loggingBrokerMock.Verify(broker =>
                 broker.LogErrorAsync(It.Is(SameExceptionAs(
                     expectedEventAddressV2DependencyValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertEventAddressV2Async(It.IsAny<EventAddressV2>()),
+                    Times.Never);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowDependencyExceptionOnAddIfDbUpdateExceptionOccursAndLogItAsync()
+        {
+            // given
+            EventAddressV2 someEventAddressV2 = CreateRandomEventAddressV2();
+            var dbUpdateException = new DbUpdateException();
+
+            var failedEventAddressV2StorageException =
+                new FailedEventAddressV2StorageException(
+                    message: "Failed event address storage error occurred, contact support.",
+                    innerException: dbUpdateException);
+
+            var expectedEventAddressV2DependencyException =
+                new EventAddressV2DependencyException(
+                    message: "Event address dependency error occurred, contact support.",
+                    innerException: failedEventAddressV2StorageException);
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetDateTimeOffsetAsync())
+                    .ThrowsAsync(dbUpdateException);
+
+            // when
+            ValueTask<EventAddressV2> addEventAddressV2Task =
+                this.eventAddressV2Service.AddEventAddressV2Async(someEventAddressV2);
+
+            EventAddressV2DependencyException actualEventAddressV2DependencyException =
+                await Assert.ThrowsAsync<EventAddressV2DependencyException>(
+                    addEventAddressV2Task.AsTask);
+
+            // then
+            actualEventAddressV2DependencyException.Should()
+                .BeEquivalentTo(expectedEventAddressV2DependencyException);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetDateTimeOffsetAsync(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedEventAddressV2DependencyException))),
                         Times.Once);
 
             this.storageBrokerMock.Verify(broker =>
