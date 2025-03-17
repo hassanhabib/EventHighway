2c8f34000c657b515bc905e1778aaaf9c0e523b0
ZafarUrakov <zafarurakov@outlook.com>
2025-03-17 14:17:53 +0800

    ShouldThrowDependencyValidationOnRemoveByIdIfDependencyValidationErrorOccursAndLogItAsync -> PASS

---
+++ EventHighway.Core.Tests.Unit/Services/Processings/ListenerEvents/V2/ListenerEventV2ProcessingServiceTests.Exceptions..RemoveById.cs
@@ -0,0 +1,61 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using EventHighway.Core.Models.Services.Foundations.ListenerEvents.V2;
+using EventHighway.Core.Models.Services.Processings.ListenerEvents.V2.Exceptions;
+using FluentAssertions;
+using Moq;
+using Xeptions;
+
+namespace EventHighway.Core.Tests.Unit.Services.Processings.ListenerEvents.V2
+{
+    public partial class ListenerEventV2ProcessingServiceTests
+    {
+        [Theory]
+        [MemberData(nameof(ListenerEventV2ValidationExceptions))]
+        public async Task ShouldThrowDependencyValidationOnRemoveByIdIfDependencyValidationErrorOccursAndLogItAsync(
+            Xeption listenerEventV2ValidationException)
+        {
+            // given
+            Guid someListenerEventV2Id = GetRandomId();
+
+            var expectedListenerEventV2ProcessingDependencyValidationException =
+                new ListenerEventV2ProcessingDependencyValidationException(
+                    message: "Listener event validation error occurred, fix the errors and try again.",
+                    innerException: listenerEventV2ValidationException.InnerException as Xeption);
+
+            this.listenerEventV2ServiceMock.Setup(service =>
+                service.RemoveListenerEventV2ByIdAsync(It.IsAny<Guid>()))
+                    .ThrowsAsync(listenerEventV2ValidationException);
+
+            // when
+            ValueTask<ListenerEventV2> removeListenerEventV2ByIdTask =
+                this.listenerEventV2ProcessingService.RemoveListenerEventV2ByIdAsync(
+                    someListenerEventV2Id);
+
+            ListenerEventV2ProcessingDependencyValidationException
+                actualListenerEventV2ProcessingDependencyValidationException =
+                    await Assert.ThrowsAsync<ListenerEventV2ProcessingDependencyValidationException>(
+                        removeListenerEventV2ByIdTask.AsTask);
+
+            // then
+            actualListenerEventV2ProcessingDependencyValidationException.Should()
+                .BeEquivalentTo(expectedListenerEventV2ProcessingDependencyValidationException);
+
+            this.listenerEventV2ServiceMock.Verify(service =>
+                service.RemoveListenerEventV2ByIdAsync(It.IsAny<Guid>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedListenerEventV2ProcessingDependencyValidationException))),
+                        Times.Once);
+
+            this.listenerEventV2ServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
