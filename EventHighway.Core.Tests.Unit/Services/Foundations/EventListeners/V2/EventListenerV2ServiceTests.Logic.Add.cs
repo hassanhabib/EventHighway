d116d86fe2aae93caf229e8fedf6cedcad6f2342
ZafarUrakov <zafarurakov@outlook.com>
2025-03-17 16:48:51 +0800

    ShouldAddEventListenerV2Async -> FAIL

---
+++ EventHighway.Core.Tests.Unit/Services/Foundations/EventListeners/V2/EventListenerV2ServiceTests.Logic.Add.cs
@@ -0,0 +1,70 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using EventHighway.Core.Models.Services.Foundations.ListenerEvents.V2;
+using Moq;
+using System.Threading.Tasks;
+using System;
+using EventHighway.Core.Models.Services.Foundations.EventListeners.V2;
+using Force.DeepCloner;
+using FluentAssertions;
+
+namespace EventHighway.Core.Tests.Unit.Services.Foundations.EventListeners.V2
+{
+    public partial class EventListenerV2ServiceTests
+    {
+        [Fact]
+        public async Task ShouldAddEventListenerV2Async()
+        {
+            // given
+            DateTimeOffset randomDateTimeOffset =
+                GetRandomDateTime();
+
+            EventListenerV2 randomEventListenerV2 =
+                CreateRandomEventListenerV2(
+                    randomDateTimeOffset);
+
+            EventListenerV2 inputEventListenerV2 =
+                randomEventListenerV2;
+
+            EventListenerV2 storageEventListenerV2 =
+                inputEventListenerV2;
+
+            EventListenerV2 expectedEventListenerV2 =
+                storageEventListenerV2.DeepClone();
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetDateTimeOffsetAsync())
+                    .ReturnsAsync(randomDateTimeOffset);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.InsertEventListenerV2Async(
+                    inputEventListenerV2))
+                        .ReturnsAsync(storageEventListenerV2);
+
+            // when
+            EventListenerV2 actualEventListenerV2 =
+                await this.eventListenerV2Service
+                    .AddEventListenerV2Async(
+                        inputEventListenerV2);
+
+            // then
+            actualEventListenerV2.Should().BeEquivalentTo(
+                expectedEventListenerV2);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetDateTimeOffsetAsync(),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertEventListenerV2Async(
+                    inputEventListenerV2),
+                        Times.Once);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
--- EventHighway.Core.Tests.Unit/Services/Foundations/EventListeners/V2/EventListenerV2ServiceTests.cs
+++ EventHighway.Core.Tests.Unit/Services/Foundations/EventListeners/V2/EventListenerV2ServiceTests.cs
@@ -8,6 +8,7 @@ using System.Linq.Expressions;
 using System.Runtime.CompilerServices;
 using EventHighway.Core.Brokers.Loggings;
 using EventHighway.Core.Brokers.Storages;
+using EventHighway.Core.Brokers.Times;
 using EventHighway.Core.Models.Services.Foundations.EventListeners.V2;
 using EventHighway.Core.Services.Foundations.EventListeners.V2;
 using Microsoft.Data.SqlClient;
@@ -21,15 +22,18 @@ namespace EventHighway.Core.Tests.Unit.Services.Foundations.EventListeners.V2
     {
         private readonly Mock<IStorageBroker> storageBrokerMock;
         private readonly Mock<ILoggingBroker> loggingBrokerMock;
+        private readonly Mock<IDateTimeBroker> dateTimeBrokerMock;
         private readonly IEventListenerV2Service eventListenerV2Service;
 
         public EventListenerV2ServiceTests()
         {
             this.storageBrokerMock = new Mock<IStorageBroker>();
             this.loggingBrokerMock = new Mock<ILoggingBroker>();
+            this.dateTimeBrokerMock = new Mock<IDateTimeBroker>();
 
             this.eventListenerV2Service = new EventListenerV2Service(
                 storageBroker: this.storageBrokerMock.Object,
+                dateTimeBroker: this.dateTimeBrokerMock.Object,
                 loggingBroker: this.loggingBrokerMock.Object);
         }
 
@@ -45,24 +49,32 @@ namespace EventHighway.Core.Tests.Unit.Services.Foundations.EventListeners.V2
         private static Guid GetRandomId() =>
             Guid.NewGuid();
 
-        private static IQueryable<EventListenerV2> CreateRandomEventListenerV2s() =>
-            CreateEventListenerV2Filler().Create(count: GetRandomNumber()).AsQueryable();
+        private static IQueryable<EventListenerV2> CreateRandomEventListenerV2s()
+        {
+            return CreateEventListenerV2Filler(
+                dates: GetRandomDateTime())
+                    .Create(count: GetRandomNumber())
+                        .AsQueryable();
+        }
 
         private static EventListenerV2 CreateRandomEventListenerV2() =>
-            CreateEventListenerV2Filler().Create();
+            CreateEventListenerV2Filler(dates: GetRandomDateTime()).Create();
+
+        private static EventListenerV2 CreateRandomEventListenerV2(DateTimeOffset dates) =>
+            CreateEventListenerV2Filler(dates).Create();
 
         private static int GetRandomNumber() =>
             new IntRange(min: 2, max: 9).GetValue();
 
-        private static DateTimeOffset CreateRandomDateTime() =>
+        private static DateTimeOffset GetRandomDateTime() =>
             new DateTimeRange(earliestDate: DateTime.UnixEpoch).GetValue();
 
-        private static Filler<EventListenerV2> CreateEventListenerV2Filler()
+        private static Filler<EventListenerV2> CreateEventListenerV2Filler(DateTimeOffset dates)
         {
             var filler = new Filler<EventListenerV2>();
 
             filler.Setup()
-                .OnType<DateTimeOffset>().Use(CreateRandomDateTime)
+                .OnType<DateTimeOffset>().Use(dates)
 
                 .OnProperty(eventListenerV2 =>
                     eventListenerV2.EventAddress).IgnoreIt()
