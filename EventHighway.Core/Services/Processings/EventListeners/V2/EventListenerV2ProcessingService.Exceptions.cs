34bacd4db789fe6b4410ab8bc0677a3e05bcb5c3
ZafarUrakov <zafarurakov@outlook.com>
2025-03-18 08:23:03 +0800

    ShouldThrowServiceExceptionOnAddIfExceptionOccursAndLogItAsync -> PASS

--- EventHighway.Core/Services/Processings/EventListeners/V2/EventListenerV2ProcessingService.Exceptions.cs
+++ EventHighway.Core/Services/Processings/EventListeners/V2/EventListenerV2ProcessingService.Exceptions.cs
@@ -54,6 +54,16 @@ namespace EventHighway.Core.Services.Processings.EventListeners.V2
                 throw await CreateAndLogDependencyExceptionAsync(
                     eventListenerV2ServiceException);
             }
+            catch (Exception exception)
+            {
+                var failedEventListenerV2ProcessingServiceException =
+                    new FailedEventListenerV2ProcessingServiceException(
+                        message: "Failed event listener service error occurred, contact support.",
+                        innerException: exception);
+
+                throw await CreateAndLogServiceExceptionAsync(
+                    failedEventListenerV2ProcessingServiceException);
+            }
         }
 
         private async ValueTask<IQueryable<EventListenerV2>> TryCatch(
