9ea7e2158ee901e128741cb95078c850c85806d5
ZafarUrakov <zafarurakov@outlook.com>
2025-03-18 08:43:17 +0800

    ShouldThrowValidationExceptionOnAddIfEventListenerV2IsNullAndLogItAsync -> FAIL

---
+++ EventHighway.Core/Models/Services/Orchestrations/EventListeners/V2/Exceptions/NullEventListenerV2OrchestrationException.cs
@@ -0,0 +1,15 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using Xeptions;
+
+namespace EventHighway.Core.Models.Services.Orchestrations.EventListeners.V2.Exceptions
+{
+    public class NullEventListenerV2OrchestrationException : Xeption
+    {
+        public NullEventListenerV2OrchestrationException(string message)
+            : base(message)
+        { }
+    }
+}
