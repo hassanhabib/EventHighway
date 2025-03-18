ce8879352690f2388ae92386a8725f2a8d399416
ZafarUrakov <zafarurakov@outlook.com>
2025-03-18 15:09:24 +0800

    ShouldThrowValidationExceptionOnRemoveByIdIfIdIsInvalidAndLogItAsync -> PASS

---
+++ EventHighway.Core/Models/Services/Coordinations/Events/V2/Exceptions/InvalidEventV2CoordinationException.cs
@@ -0,0 +1,15 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using Xeptions;
+
+namespace EventHighway.Core.Models.Services.Coordinations.Events.V2.Exceptions
+{
+    public class InvalidEventV2CoordinationException : Xeption
+    {
+        public InvalidEventV2CoordinationException(string message)
+            : base(message)
+        { }
+    }
+}
