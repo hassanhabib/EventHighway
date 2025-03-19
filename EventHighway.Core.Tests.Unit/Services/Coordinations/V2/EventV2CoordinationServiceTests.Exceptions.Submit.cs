edfff56b402b8d1b247188cd5c4a36a6261d3f1e
ZafarUrakov <zafarurakov@outlook.com>
2025-03-19 16:47:12 +0800

    ShouldThrowDependencyValidationOnSubmitIfDependencyValidationErrorOccursAndLogItAsync -> FAIL

---
+++ EventHighway.Core.Tests.Unit/Services/Coordinations/V2/EventV2CoordinationServiceTests.Exceptions.Submit.cs
@@ -0,0 +1,89 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using EventHighway.Core.Models.Services.Coordinations.Events.V2.Exceptions;
+using EventHighway.Core.Models.Services.Foundations.EventCall.V2;
+using EventHighway.Core.Models.Services.Foundations.Events.V2;
+using EventHighway.Core.Models.Services.Foundations.ListenerEvents.V2;
+using FluentAssertions;
+using Moq;
+using Xeptions;
+
+namespace EventHighway.Core.Tests.Unit.Services.Coordinations.V2
+{
+    public partial class EventV2CoordinationServiceTests
+    {
+        [Theory]
+        [MemberData(nameof(EventV2ValidationExceptions))]
+        [MemberData(nameof(EventListenerV2ValidationExceptions))]
+        public async Task ShouldThrowDependencyValidationOnSubmitIfDependencyValidationErrorOccursAndLogItAsync(
+            Xeption validationException)
+        {
+            // given
+            EventV2 someEventV2 = CreateRandomEventV2();
+
+            var expectedEventV2CoordinationDependencyValidationException =
+                new EventV2CoordinationDependencyValidationException(
+                    message: "Event validation error occurred, fix the errors and try again.",
+                    innerException: validationException.InnerException as Xeption);
+
+            this.dateTimeBrokerMock.Setup(service =>
+                service.GetDateTimeOffsetAsync())
+                    .ThrowsAsync(validationException);
+
+            // when
+            ValueTask<EventV2> submitEventV2Task =
+                this.eventV2CoordinationService.SubmitEventV2Async(someEventV2);
+
+            EventV2CoordinationDependencyValidationException
+                actualEventV2CoordinationDependencyValidationException =
+                    await Assert.ThrowsAsync<EventV2CoordinationDependencyValidationException>(
+                        submitEventV2Task.AsTask);
+
+            // then
+            expectedEventV2CoordinationDependencyValidationException.Should()
+                .BeEquivalentTo(expectedEventV2CoordinationDependencyValidationException);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetDateTimeOffsetAsync(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedEventV2CoordinationDependencyValidationException))),
+                        Times.Once);
+
+            this.eventListenerV2OrchestrationServiceMock.Verify(service =>
+                service.RetrieveEventListenerV2sByEventAddressIdAsync(
+                    It.IsAny<Guid>()),
+                        Times.Never);
+
+            this.eventV2OrchestrationServiceMock.Verify(service =>
+                service.SubmitEventV2Async(It.IsAny<EventV2>()),
+                    Times.Never);
+
+            this.eventListenerV2OrchestrationServiceMock.Verify(service =>
+                service.AddListenerEventV2Async(
+                    It.IsAny<ListenerEventV2>()),
+                        Times.Never);
+
+            this.eventV2OrchestrationServiceMock.Verify(service =>
+                service.RunEventCallV2Async(
+                    It.IsAny<EventCallV2>()),
+                        Times.Never);
+
+            this.eventListenerV2OrchestrationServiceMock.Verify(service =>
+                service.ModifyListenerEventV2Async(
+                    It.IsAny<ListenerEventV2>()),
+                        Times.Never);
+
+            this.eventV2OrchestrationServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.eventListenerV2OrchestrationServiceMock.VerifyNoOtherCalls();
+        }
+    }
+}
