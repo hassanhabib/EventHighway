f4385a4676fc0f6ef7d8c3ed589047079a4ba6e0
ZafarUrakov <zafarurakov@outlook.com>
2025-03-19 08:31:44 +0800

    ShouldThrowValidationExceptionOnAddIfEventV2IsNullAndLogItAsync -> FAIL

---
+++ EventHighway.Core/Models/Services/Processings/Events/V2/Exceptions/NullEventV2ProcessingException.cs
@@ -0,0 +1,15 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using Xeptions;
+
+namespace EventHighway.Core.Models.Services.Processings.Events.V2.Exceptions
+{
+    public class NullEventV2ProcessingException : Xeption
+    {
+        public NullEventV2ProcessingException(string message)
+            : base(message)
+        { }
+    }
+}
