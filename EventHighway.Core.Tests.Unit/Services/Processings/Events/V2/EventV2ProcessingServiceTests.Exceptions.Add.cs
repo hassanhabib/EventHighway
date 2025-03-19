ae80d662a874dd523946d1fc3ce7b21b88b1af36
ZafarUrakov <zafarurakov@outlook.com>
2025-03-19 08:35:10 +0800

    ShouldThrowServiceExceptionOnAddIfExceptionOccursAndLogItAsync -> PASS

--- EventHighway.Core.Tests.Unit/Services/Processings/Events/V2/EventV2ProcessingServiceTests.Exceptions.Add.cs
+++ EventHighway.Core.Tests.Unit/Services/Processings/Events/V2/EventV2ProcessingServiceTests.Exceptions.Add.cs
@@ -2,6 +2,7 @@
 // Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
 // ----------------------------------------------------------------------------------
 
+using System;
 using System.Threading.Tasks;
 using EventHighway.Core.Models.Services.Foundations.Events.V2;
 using EventHighway.Core.Models.Services.Processings.Events.V2.Exceptions;
@@ -93,6 +94,54 @@ namespace EventHighway.Core.Tests.Unit.Services.Processings.Events.V2
             this.loggingBrokerMock.Verify(broker =>
                 broker.LogErrorAsync(It.Is(SameExceptionAs(
                     expectedEventV2ProcessingDependencyException))),
+                        Times.Once);
+
+            this.eventV2ServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowServiceExceptionOnAddIfExceptionOccursAndLogItAsync()
+        {
+            // given
+            EventV2 someEventV2 = CreateRandomEventV2();
+            var serviceException = new Exception();
+
+            var failedEventV2ProcessingServiceException =
+                new FailedEventV2ProcessingServiceException(
+                    message: "Failed event service error occurred, contact support.",
+                    innerException: serviceException);
+
+            var expectedEventV2ProcessingExceptionException =
+                new EventV2ProcessingServiceException(
+                    message: "Event service error occurred, contact support.",
+                    innerException: failedEventV2ProcessingServiceException);
+
+            this.eventV2ServiceMock.Setup(service =>
+                service.AddEventV2Async(It.IsAny<EventV2>()))
+                    .ThrowsAsync(serviceException);
+
+            // when
+            ValueTask<EventV2> addEventV2Task =
+                this.eventV2ProcessingService.AddEventV2Async(
+                    someEventV2);
+
+            EventV2ProcessingServiceException
+                actualEventV2ProcessingServiceException =
+                    await Assert.ThrowsAsync<EventV2ProcessingServiceException>(
+                        addEventV2Task.AsTask);
+
+            // then
+            actualEventV2ProcessingServiceException.Should()
+                .BeEquivalentTo(expectedEventV2ProcessingExceptionException);
+
+            this.eventV2ServiceMock.Verify(service =>
+                service.AddEventV2Async(It.IsAny<EventV2>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedEventV2ProcessingExceptionException))),
                         Times.Once);
 
             this.eventV2ServiceMock.VerifyNoOtherCalls();
