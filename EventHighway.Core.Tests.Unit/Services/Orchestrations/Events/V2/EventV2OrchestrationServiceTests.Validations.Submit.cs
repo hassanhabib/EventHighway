4a4f7afe5ce65b10eed7936b9d33d6950ca39c4f
ZafarUrakov <zafarurakov@outlook.com>
2025-03-19 16:58:57 +0800

    ShouldThrowValidationExceptionOnSubmitIfEventV2IsNullAndLogItAsync -> FAIL

---
+++ EventHighway.Core.Tests.Unit/Services/Orchestrations/Events/V2/EventV2OrchestrationServiceTests.Validations.Submit.cs
@@ -0,0 +1,63 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using EventHighway.Core.Models.Services.Foundations.Events.V2;
+using EventHighway.Core.Models.Services.Orchestrations.Events.V2.Exceptions;
+using FluentAssertions;
+using Moq;
+
+namespace EventHighway.Core.Tests.Unit.Services.Orchestrations.Events.V2
+{
+    public partial class EventV2OrchestrationServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnSubmitIfEventV2IsNullAndLogItAsync()
+        {
+            // given
+            EventV2 nullEventV2 = null;
+
+            var nullEventV2OrchestrationException =
+                new NullEventV2OrchestrationException(message: "Event is null.");
+
+            var expectedEventV2OrchestrationValidationException =
+                new EventV2OrchestrationValidationException(
+                    message: "Event validation error occurred, fix the errors and try again.",
+                    innerException: nullEventV2OrchestrationException);
+
+            // when
+            ValueTask<EventV2> submitEventV2Task =
+                this.eventV2OrchestrationService.SubmitEventV2Async(nullEventV2);
+
+            EventV2OrchestrationValidationException
+                actualEventV2OrchestrationValidationException =
+                    await Assert.ThrowsAsync<EventV2OrchestrationValidationException>(
+                        submitEventV2Task.AsTask);
+
+            // then
+            actualEventV2OrchestrationValidationException.Should().BeEquivalentTo(
+                expectedEventV2OrchestrationValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedEventV2OrchestrationValidationException))),
+                        Times.Once);
+
+            this.eventAddressV2ProcessingServiceMock.Verify(service =>
+                service.RetrieveEventAddressV2ByIdAsync(It.IsAny<Guid>()),
+                    Times.Never);
+
+            this.eventV2ProcessingServiceMock.Verify(broker =>
+                broker.AddEventV2Async(
+                    It.IsAny<EventV2>()),
+                        Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.eventAddressV2ProcessingServiceMock.VerifyNoOtherCalls();
+            this.eventV2ProcessingServiceMock.VerifyNoOtherCalls();
+            this.eventCallV2ProcessingServiceMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ EventHighway.Core/Models/Services/Orchestrations/Events/V2/Exceptions/NullEventV2OrchestrationException.cs
@@ -0,0 +1,15 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using Xeptions;
+
+namespace EventHighway.Core.Models.Services.Orchestrations.Events.V2.Exceptions
+{
+    public class NullEventV2OrchestrationException : Xeption
+    {
+        public NullEventV2OrchestrationException(string message)
+            : base(message)
+        { }
+    }
+}
