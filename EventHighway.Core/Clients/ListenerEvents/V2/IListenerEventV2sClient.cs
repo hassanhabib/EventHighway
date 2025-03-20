7d2b9a77ba7ec5cf7035493b7905bb673ae5db14
ZafarUrakov <zafarurakov@outlook.com>
2025-03-20 22:57:19 +0800

    ShouldRetrieveAllListenerEventV2sAsync -> PASS

---
+++ EventHighway.Core/Clients/ListenerEvents/V2/IListenerEventV2sClient.cs
@@ -0,0 +1,15 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using System.Linq;
+using System.Threading.Tasks;
+using EventHighway.Core.Models.Services.Foundations.ListenerEvents.V2;
+
+namespace EventHighway.Core.Clients.ListenerEvents.V2
+{
+    public interface IListenerEventV2sClient
+    {
+        ValueTask<IQueryable<ListenerEventV2>> RetrieveAllListenerEventV2sAsync();
+    }
+}
---
+++ EventHighway.Core/Clients/ListenerEvents/V2/ListenerEventV2sClient.cs
@@ -0,0 +1,25 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using System.Linq;
+using System.Threading.Tasks;
+using EventHighway.Core.Models.Services.Foundations.ListenerEvents.V2;
+using EventHighway.Core.Services.Orchestrations.EventListeners.V2;
+
+namespace EventHighway.Core.Clients.ListenerEvents.V2
+{
+    internal class ListenerEventV2sClient : IListenerEventV2sClient
+    {
+        private readonly IEventListenerV2OrchestrationService eventListenerV2OrchestrationService;
+
+        public ListenerEventV2sClient(IEventListenerV2OrchestrationService eventListenerV2OrchestrationService) =>
+            this.eventListenerV2OrchestrationService = eventListenerV2OrchestrationService;
+
+        public async ValueTask<IQueryable<ListenerEventV2>> RetrieveAllListenerEventV2sAsync()
+        {
+            return await this.eventListenerV2OrchestrationService
+                .RetrieveAllListenerEventV2sAsync();
+        }
+    }
+}
