56d8219f8952d7d1bc847dd3fd0f1a9b3aee6105
ZafarUrakov <zafarurakov@outlook.com>
2025-03-20 17:56:29 +0800

    ShouldRegisterEventAddressV2Async -> FAIL

---
+++ EventHighway.Core.Tests.Unit/Clients/EventAddresses/V2/EventAddressesV2ClientTests.Logic.Register.cs
@@ -0,0 +1,54 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using System.Threading.Tasks;
+using EventHighway.Core.Models.Services.Foundations.EventAddresses.V2;
+using FluentAssertions;
+using Force.DeepCloner;
+using Moq;
+
+namespace EventHighway.Core.Tests.Unit.Clients.EventAddresses.V2
+{
+    public partial class EventAddressesV2ClientTests
+    {
+        [Fact]
+        public async Task ShouldRegisterEventAddressV2Async()
+        {
+            // given
+            EventAddressV2 randomEventAddressV2 =
+                CreateRandomEventAddressV2();
+
+            EventAddressV2 inputEventAddressV2 =
+                randomEventAddressV2;
+
+            EventAddressV2 registeredEventAddressV2 =
+                inputEventAddressV2;
+
+            EventAddressV2 expectedEventAddressV2 =
+                registeredEventAddressV2.DeepClone();
+
+            this.eventAddressV2ServiceMock.Setup(service =>
+                service.AddEventAddressV2Async(
+                    inputEventAddressV2))
+                        .ReturnsAsync(registeredEventAddressV2);
+
+            // when
+            EventAddressV2 actualEventAddressV2 =
+                await this.eventAddressesClient
+                    .RegisterEventAddressV2Async(
+                        inputEventAddressV2);
+
+            // then
+            actualEventAddressV2.Should()
+                .BeEquivalentTo(expectedEventAddressV2);
+
+            this.eventAddressV2ServiceMock.Verify(service =>
+                service.AddEventAddressV2Async(
+                    inputEventAddressV2),
+                        Times.Once);
+
+            this.eventAddressV2ServiceMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ EventHighway.Core.Tests.Unit/Clients/EventAddresses/V2/EventAddressesV2ClientTests.cs
@@ -0,0 +1,54 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using System;
+using EventHighway.Core.Clients.EventAddresses.V2;
+using EventHighway.Core.Models.Services.Foundations.EventAddresses.V2;
+using EventHighway.Core.Services.Foundations.EventAddresses.V2;
+using Moq;
+using Tynamix.ObjectFiller;
+
+namespace EventHighway.Core.Tests.Unit.Clients.EventAddresses.V2
+{
+    public partial class EventAddressesV2ClientTests
+    {
+        private readonly Mock<IEventAddressV2Service> eventAddressV2ServiceMock;
+        private readonly IEventAddressesV2Client eventAddressesClient;
+
+        public EventAddressesV2ClientTests()
+        {
+            this.eventAddressV2ServiceMock =
+                new Mock<IEventAddressV2Service>();
+
+            this.eventAddressesClient =
+                new EventAddressesV2Client(
+                    eventAddressV2Service: this.eventAddressV2ServiceMock.Object);
+        }
+
+        private static DateTimeOffset GetRandomDateTimeOffset() =>
+            new DateTimeRange(earliestDate: DateTime.UnixEpoch).GetValue();
+
+        private static EventAddressV2 CreateRandomEventAddressV2() =>
+            CreateEventAddressV2Filler().Create();
+
+        private static Filler<EventAddressV2> CreateEventAddressV2Filler()
+        {
+            var filler = new Filler<EventAddressV2>();
+
+            filler.Setup()
+                .OnType<DateTimeOffset>().Use(GetRandomDateTimeOffset)
+
+                .OnProperty(eventAddressV2 => eventAddressV2.Events)
+                    .IgnoreIt()
+
+                .OnProperty(eventAddressV2 => eventAddressV2.EventListeners)
+                    .IgnoreIt()
+
+                .OnProperty(eventAddressV2 => eventAddressV2.ListenerEvents)
+                    .IgnoreIt();
+
+            return filler;
+        }
+    }
+}
