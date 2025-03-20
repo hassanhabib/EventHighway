c5e6635ce424cfcb7d32f3148be72f6972859c4a
ZafarUrakov <zafarurakov@outlook.com>
2025-03-20 21:52:25 +0800

    ShouldThrowDependencyExceptionOnRemoveByIdIfDependencyErrorOccursAsync -> FAIL

--- EventHighway.Core.Tests.Unit/Clients/EventAddresses/V2/EventAddressesV2ClientTests.Exceptions.RemoveById.cs
+++ EventHighway.Core.Tests.Unit/Clients/EventAddresses/V2/EventAddressesV2ClientTests.Exceptions.RemoveById.cs
@@ -2,12 +2,13 @@
 // Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
 // ----------------------------------------------------------------------------------
 
-using EventHighway.Core.Models.Clients.EventAddresses.V2.Exceptions;
-using EventHighway.Core.Models.Services.Foundations.EventAddresses.V2;
-using FluentAssertions;
-using Moq;
 using System;
 using System.Threading.Tasks;
+using EventHighway.Core.Models.Clients.EventAddresses.V2.Exceptions;
+using EventHighway.Core.Models.Services.Foundations.EventAddresses.V2;
+using EventHighway.Core.Models.Services.Foundations.EventAddresses.V2.Exceptions;
+using FluentAssertions;
+using Moq;
 using Xeptions;
 
 namespace EventHighway.Core.Tests.Unit.Clients.EventAddresses.V2
@@ -43,6 +44,48 @@ namespace EventHighway.Core.Tests.Unit.Clients.EventAddresses.V2
             // then
             actualEventAddressV2ClientDependencyValidationException.Should()
                 .BeEquivalentTo(expectedEventAddressV2ClientDependencyValidationException);
+
+            this.eventAddressV2ServiceMock.Verify(service =>
+                service.RemoveEventAddressV2ByIdAsync(It.IsAny<Guid>()),
+                    Times.Once);
+
+            this.eventAddressV2ServiceMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowDependencyExceptionOnRemoveByIdIfDependencyErrorOccursAsync()
+        {
+            // given
+            Guid someEventAddressV2Id = GetRandomId();
+            string someMessage = GetRandomString();
+            var someInnerException = new Xeption();
+
+            var eventV2DependencyException =
+                new EventAddressV2DependencyException(
+                    someMessage,
+                    someInnerException);
+
+            var expectedEventAddressV2ClientDependencyException =
+                new EventAddressV2ClientDependencyException(
+                    message: "Event address client dependency error occurred, contact support.",
+                    innerException: eventV2DependencyException.InnerException as Xeption);
+
+            this.eventAddressV2ServiceMock.Setup(service =>
+                service.RemoveEventAddressV2ByIdAsync(It.IsAny<Guid>()))
+                    .ThrowsAsync(eventV2DependencyException);
+
+            // when
+            ValueTask<EventAddressV2> removeEventAddressV2ByIdTask =
+                this.eventAddressesClient.RemoveEventAddressV2ByIdAsync(someEventAddressV2Id);
+
+            EventAddressV2ClientDependencyException
+                actualEventAddressV2ClientDependencyException =
+                    await Assert.ThrowsAsync<EventAddressV2ClientDependencyException>(
+                        removeEventAddressV2ByIdTask.AsTask);
+
+            // then
+            actualEventAddressV2ClientDependencyException.Should()
+                .BeEquivalentTo(expectedEventAddressV2ClientDependencyException);
 
             this.eventAddressV2ServiceMock.Verify(service =>
                 service.RemoveEventAddressV2ByIdAsync(It.IsAny<Guid>()),
