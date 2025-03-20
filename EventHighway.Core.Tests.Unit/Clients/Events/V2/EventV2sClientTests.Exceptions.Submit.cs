bbebc6585b1338fa18c02777a0d9eb02f15bd3ed
ZafarUrakov <zafarurakov@outlook.com>
2025-03-20 16:53:00 +0800

    ShouldThrowDependencyExceptionOnSubmitIfDependencyErrorOccursAsync -> FAIL

--- EventHighway.Core.Tests.Unit/Clients/Events/V2/EventV2sClientTests.Exceptions.Submit.cs
+++ EventHighway.Core.Tests.Unit/Clients/Events/V2/EventV2sClientTests.Exceptions.Submit.cs
@@ -56,5 +56,48 @@ namespace EventHighway.Core.Tests.Unit.Clients.Events.V2
 
             this.eventV2CoordinationServiceMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldThrowDependencyExceptionOnSubmitIfDependencyErrorOccursAsync()
+        {
+            // given
+            EventV2 someEventV2 = CreateRandomEventV2();
+            string someMessage = GetRandomString();
+            var someInnerException = new Xeption();
+
+            var eventV2CoordinationDependencyException =
+                new EventV2CoordinationDependencyException(
+                    someMessage,
+                    someInnerException);
+
+            var expectedEventV2ClientDependencyException =
+                new EventV2ClientDependencyException(
+                    message: "Event client dependency error occurred, contact support.",
+
+                    innerException: eventV2CoordinationDependencyException
+                        .InnerException as Xeption);
+
+            this.eventV2CoordinationServiceMock.Setup(service =>
+                service.SubmitEventV2Async(It.IsAny<EventV2>()))
+                    .ThrowsAsync(eventV2CoordinationDependencyException);
+
+            // when
+            ValueTask<EventV2> submitEventV2Task =
+                this.eventV2SClient.SubmitEventV2Async(someEventV2);
+
+            EventV2ClientDependencyException actualEventV2ClientDependencyException =
+                await Assert.ThrowsAsync<EventV2ClientDependencyException>(
+                    submitEventV2Task.AsTask);
+
+            // then
+            actualEventV2ClientDependencyException.Should()
+                .BeEquivalentTo(expectedEventV2ClientDependencyException);
+
+            this.eventV2CoordinationServiceMock.Verify(service =>
+                service.SubmitEventV2Async(It.IsAny<EventV2>()),
+                    Times.Once);
+
+            this.eventV2CoordinationServiceMock.VerifyNoOtherCalls();
+        }
     }
 }
