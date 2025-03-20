df0e702c34e2a879ae4617d85a3fb6d5409ff7ae
ZafarUrakov <zafarurakov@outlook.com>
2025-03-20 16:43:37 +0800

    ShouldSubmitEventV2Async -> FAIL

--- EventHighway.Core.Tests.Unit/Clients/Events/V2/EventV2sClientTests.cs
+++ EventHighway.Core.Tests.Unit/Clients/Events/V2/EventV2sClientTests.cs
@@ -2,7 +2,9 @@
 // Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
 // ----------------------------------------------------------------------------------
 
+using System;
 using EventHighway.Core.Clients.Events.V2;
+using EventHighway.Core.Models.Services.Foundations.Events.V2;
 using EventHighway.Core.Services.Coordinations.Events.V2;
 using Moq;
 using Tynamix.ObjectFiller;
@@ -27,5 +29,31 @@ namespace EventHighway.Core.Tests.Unit.Clients.Events.V2
 
         private static string GetRandomString() =>
             new MnemonicString().GetValue();
+
+        private static DateTimeOffset GetRandomDateTimeOffset() =>
+            new DateTimeRange(earliestDate: DateTime.UnixEpoch).GetValue();
+
+        private static EventV2 CreateRandomEventV2() =>
+            CreateEventV2Filler().Create();
+
+        private static Filler<EventV2> CreateEventV2Filler()
+        {
+            var filler = new Filler<EventV2>();
+
+            filler.Setup()
+                .OnType<DateTimeOffset>()
+                    .Use(GetRandomDateTimeOffset)
+
+                .OnType<DateTimeOffset?>()
+                    .Use(GetRandomDateTimeOffset())
+
+                .OnProperty(eventV2 =>
+                    eventV2.EventAddress).IgnoreIt()
+
+                .OnProperty(eventV2 =>
+                    eventV2.ListenerEvents).IgnoreIt();
+
+            return filler;
+        }
     }
 }
