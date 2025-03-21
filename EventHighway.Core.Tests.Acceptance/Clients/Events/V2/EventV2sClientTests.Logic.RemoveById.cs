61cd08b807c10c5c543a6fe9c80c7a2f450eab9c
ZafarUrakov <zafarurakov@outlook.com>
2025-03-22 07:45:04 +0800

    ShouldRemoveEventV2ByIdAsync -> PASS

---
+++ EventHighway.Core.Tests.Acceptance/Clients/Events/V2/EventV2sClientTests.Logic.RemoveById.cs
@@ -0,0 +1,57 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using EventHighway.Core.Models.Services.Foundations.EventAddresses.V2;
+using EventHighway.Core.Models.Services.Foundations.Events.V2;
+using FluentAssertions;
+
+namespace EventHighway.Core.Tests.Acceptance.Clients.Events.V2
+{
+    public partial class EventV2sClientTests
+    {
+        [Fact]
+        public async Task ShouldRemoveEventV2ByIdAsync()
+        {
+            // given
+            EventAddressV2 randomEventAddressV2 =
+                await CreateRandomEventAddressV2Async();
+
+            EventAddressV2 inputEventAddressV2 =
+                randomEventAddressV2;
+
+            Guid inputEventAddressV2Id =
+                inputEventAddressV2.Id;
+
+            int randomSeconds = GetRandomNumber();
+
+            DateTimeOffset scheduledDate =
+                DateTimeOffset.Now
+                    .AddSeconds(randomSeconds);
+
+            EventV2 randomEventV2 =
+                await SubmitEventV2Async(
+                    inputEventAddressV2Id,
+                    scheduledDate);
+
+            Guid inputEventV2Id = randomEventV2.Id;
+            EventV2 expectedEventV2 = randomEventV2;
+
+            // when 
+            EventV2 actualEventV2 =
+                await this.clientBroker
+                    .RemoveEventV2ByIdAsync(
+                        inputEventV2Id);
+
+            // then
+            actualEventV2.Should()
+                .BeEquivalentTo(expectedEventV2);
+
+            await this.clientBroker
+                .RemoveEventAddressV2ByIdAsync(
+                    inputEventAddressV2Id);
+        }
+    }
+}
