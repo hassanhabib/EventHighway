a4344f67b36019cb73d61f40de2479fd54152d46
ZafarUrakov <zafarurakov@outlook.com>
2025-03-21 09:12:33 +0800

    ShouldBeTrue -> PASS

---
+++ EventHighway.Core.Tests.Acceptance/DeleteMe.cs
@@ -0,0 +1,13 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+namespace EventHighway.Core.Tests.Acceptance
+{
+    public class DeleteMe
+    {
+        [Fact]
+        public void ShouldBeTrue() =>
+            Assert.True(condition: true);
+    }
+}
