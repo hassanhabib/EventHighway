8da01c856710c89eb242cd4e8db6e510be6ac1a5
ZafarUrakov <zafarurakov@outlook.com>
2025-03-21 09:15:34 +0800

    CONFIGS: Register Dependencies

--- EventHighway.Core/Clients/EventHighways/IEventHighwayClient.cs
+++ EventHighway.Core/Clients/EventHighways/IEventHighwayClient.cs
@@ -3,15 +3,23 @@
 // ----------------------------------------------------------------------------------
 
 using EventHighway.Core.Clients.EventAddresses;
+using EventHighway.Core.Clients.EventAddresses.V2;
 using EventHighway.Core.Clients.EventListeners;
+using EventHighway.Core.Clients.EventListeners.V2;
 using EventHighway.Core.Clients.Events;
+using EventHighway.Core.Clients.Events.V2;
+using EventHighway.Core.Clients.ListenerEvents.V2;
 
 namespace EventHighway.Core.Clients.EventHighways
 {
     public interface IEventHighwayClient
     {
-        public IEventAddressesClient EventAddresses { get;}
-        public IEventListenersClient EventListeners { get;}
-        public IEventsClient Events { get;}
+        public IEventAddressesClient EventAddresses { get; }
+        public IEventListenersClient EventListeners { get; }
+        public IEventsClient Events { get; }
+        public IEventV2sClient EventV2s { get; set; }
+        public IEventAddressesV2Client IEventAddressV2s { get; set; }
+        public IEventListenerV2sClient EventListenerV2s { get; set; }
+        public IListenerEventV2sClient ListenerEventV2s { get; set; }
     }
 }
