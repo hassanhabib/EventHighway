e77128346c084f8d827008d4bbafb28e9c24e1fb
ZafarUrakov <zafarurakov@outlook.com>
2025-03-25 17:57:18 +0800

    ShouldThrowServiceExceptionOnRetrieveAllIfServiceErrorOccursAsync -> PASS

--- EventHighway.Core.Tests.Unit/Clients/EventAddresses/V1/EventAddressesV1ClientTests.Exceptions.RetrieveAll.cs
+++ EventHighway.Core.Tests.Unit/Clients/EventAddresses/V1/EventAddressesV1ClientTests.Exceptions.RetrieveAll.cs
@@ -2,15 +2,13 @@
 // Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
 // ----------------------------------------------------------------------------------
 
+using System.Linq;
+using System.Threading.Tasks;
 using EventHighway.Core.Models.Clients.EventAddresses.V1.Exceptions;
-using EventHighway.Core.Models.Clients.ListenerEvents.V1.Exceptions;
 using EventHighway.Core.Models.Services.Foundations.EventAddresses.V1;
-using EventHighway.Core.Models.Services.Foundations.ListenerEvents.V1;
 using EventHighway.Core.Models.Services.Orchestrations.EventListeners.V1.Exceptions;
 using FluentAssertions;
 using Moq;
-using System.Linq;
-using System.Threading.Tasks;
 using Xeptions;
 
 namespace EventHighway.Core.Tests.Unit.Clients.EventAddresses.V1
@@ -51,6 +49,48 @@ namespace EventHighway.Core.Tests.Unit.Clients.EventAddresses.V1
             // then
             actualEventAddressV1ClientDependencyException.Should()
                 .BeEquivalentTo(expectedEventAddressV1ClientDependencyException);
+
+            this.eventAddressV1ServiceMock.Verify(service =>
+                service.RetrieveAllEventAddressV1sAsync(),
+                    Times.Once);
+
+            this.eventAddressV1ServiceMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowServiceExceptionOnRetrieveAllIfServiceErrorOccursAsync()
+        {
+            // given
+            string someMessage = GetRandomString();
+            var someInnerException = new Xeption();
+
+            var EventListenerV1OrchestrationServiceException =
+                new EventListenerV1OrchestrationServiceException(
+                    someMessage,
+                    someInnerException);
+
+            var expectedEventAddressV1ClientServiceException =
+                new EventAddressV1ClientServiceException(
+                    message: "Event address client service error occurred, contact support.",
+
+                    innerException: EventListenerV1OrchestrationServiceException
+                        .InnerException as Xeption);
+
+            this.eventAddressV1ServiceMock.Setup(service =>
+                service.RetrieveAllEventAddressV1sAsync())
+                    .ThrowsAsync(EventListenerV1OrchestrationServiceException);
+
+            // when
+            ValueTask<IQueryable<EventAddressV1>> retrieveAllEventAddressV1sTask =
+                this.eventAddressV1sClient.RetrieveAllEventAddressV1sAsync();
+
+            EventAddressV1ClientServiceException actualEventAddressV1ClientServiceException =
+                await Assert.ThrowsAsync<EventAddressV1ClientServiceException>(
+                    retrieveAllEventAddressV1sTask.AsTask);
+
+            // then
+            actualEventAddressV1ClientServiceException.Should()
+                .BeEquivalentTo(expectedEventAddressV1ClientServiceException);
 
             this.eventAddressV1ServiceMock.Verify(service =>
                 service.RetrieveAllEventAddressV1sAsync(),
