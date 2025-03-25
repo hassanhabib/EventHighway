c2619c3d29599a4c672a391461427dacb5623fee
ZafarUrakov <zafarurakov@outlook.com>
2025-03-25 17:28:43 +0800

    BROKERS: Select All Addresses V1

--- EventHighway.Core/Brokers/Storages/StorageBroker.EventAddressV1s.cs
+++ EventHighway.Core/Brokers/Storages/StorageBroker.EventAddressV1s.cs
@@ -3,8 +3,10 @@
 // ----------------------------------------------------------------------------------
 
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 using EventHighway.Core.Models.Services.Foundations.EventAddresses.V1;
+using EventHighway.Core.Models.Services.Foundations.EventListeners.V1;
 using Microsoft.EntityFrameworkCore;
 
 namespace EventHighway.Core.Brokers.Storages
@@ -15,6 +17,9 @@ namespace EventHighway.Core.Brokers.Storages
 
         public async ValueTask<EventAddressV1> InsertEventAddressV1Async(EventAddressV1 eventAddressV1) =>
             await InsertAsync(eventAddressV1);
+
+        public async ValueTask<IQueryable<EventAddressV1>> SelectAllEventAddressV1sAsync() =>
+            SelectAll<EventAddressV1>();
 
         public async ValueTask<EventAddressV1> SelectEventAddressV1ByIdAsync(Guid eventAddressV1Id) =>
             await SelectAsync<EventAddressV1>(eventAddressV1Id);
