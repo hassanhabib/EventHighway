913c09379ff212f7125191a8bae49ba7cfafcdc6
Abdulsamad Osunlana <osunlanaabdulsamad@gmail.com>
2025-05-23 03:00:26 +0100

    ShouldThrowDependencyValidationErrorOnModifyIfDependencyValidationErrorOccursAndLogItAsync -> PASS

---
+++ EventHighway.Core.Tests.Unit/Services/Processings/Events/V1/EventV1ProcessingServiceTests.Exceptions.Modify.cs
@@ -0,0 +1,59 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using System.Threading.Tasks;
+using EventHighway.Core.Models.Services.Foundations.Events.V1;
+using EventHighway.Core.Models.Services.Processings.Events.V1.Exceptions;
+using FluentAssertions;
+using Moq;
+using Xeptions;
+
+namespace EventHighway.Core.Tests.Unit.Services.Processings.Events.V1
+{
+    public partial class EventV1ProcessingServiceTests
+    {
+        [Theory]
+        [MemberData(nameof(ValidationExceptions))]
+        public async Task ShouldThrowDependencyValidationErrorOnModifyIfDependencyValidationErrorOccursAndLogItAsync(
+            Xeption validationException)
+        {
+            // given
+            EventV1 someEventV1 = CreateRandomEventV1();
+
+            var expectedEventV1ProcessingDependencyValidationException =
+                new EventV1ProcessingDependencyValidationException(
+                    message: "Event validation error occurred, fix the errors and try again.",
+                    innerException: validationException.InnerException as Xeption);
+
+            this.eventV1ServiceMock.Setup(service =>
+                service.ModifyEventV1Async(It.IsAny<EventV1>()))
+                    .ThrowsAsync(validationException);
+
+            // when
+            ValueTask<EventV1> modifyEventV1Task =
+                this.eventV1ProcessingService.ModifyEventV1Async(someEventV1);
+
+            EventV1ProcessingDependencyValidationException
+                actualEventV1ProcessingDependencyValidationException =
+                    await Assert.ThrowsAsync<EventV1ProcessingDependencyValidationException>(
+                        modifyEventV1Task.AsTask);
+
+            // then
+            actualEventV1ProcessingDependencyValidationException.Should()
+                .BeEquivalentTo(expectedEventV1ProcessingDependencyValidationException);
+
+            this.eventV1ServiceMock.Verify(service =>
+                service.ModifyEventV1Async(It.IsAny<EventV1>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedEventV1ProcessingDependencyValidationException))),
+                        Times.Once);
+
+            this.eventV1ServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
