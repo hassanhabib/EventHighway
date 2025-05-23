5be0ca09935c3e8793a938699b1b518a2d77db56
Abdulsamad Osunlana <osunlanaabdulsamad@gmail.com>
2025-05-23 03:01:03 +0100

    ShouldThrowDependencyExceptionOnModifyIfDependencyExceptionOccursAndLogItAsync -> PASS

--- EventHighway.Core.Tests.Unit/Services/Processings/Events/V1/EventV1ProcessingServiceTests.Exceptions.Modify.cs
+++ EventHighway.Core.Tests.Unit/Services/Processings/Events/V1/EventV1ProcessingServiceTests.Exceptions.Modify.cs
@@ -55,5 +55,48 @@ namespace EventHighway.Core.Tests.Unit.Services.Processings.Events.V1
             this.eventV1ServiceMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Theory]
+        [MemberData(nameof(DependencyExceptions))]
+        public async Task ShouldThrowDependencyExceptionOnModifyIfDependencyExceptionOccursAndLogItAsync(
+            Xeption dependencyException)
+        {
+            // given
+            EventV1 someEventV1 = CreateRandomEventV1();
+
+            var expectedEventV1ProcessingDependencyException =
+                new EventV1ProcessingDependencyException(
+                    message: "Event dependency error occurred, contact support.",
+                    innerException: dependencyException.InnerException as Xeption);
+
+            this.eventV1ServiceMock.Setup(service =>
+                service.ModifyEventV1Async(It.IsAny<EventV1>()))
+                    .ThrowsAsync(dependencyException);
+
+            // when
+            ValueTask<EventV1> modifyEventV1Task =
+                this.eventV1ProcessingService.ModifyEventV1Async(someEventV1);
+
+            EventV1ProcessingDependencyException
+                actualEventV1ProcessingDependencyException =
+                    await Assert.ThrowsAsync<EventV1ProcessingDependencyException>(
+                        modifyEventV1Task.AsTask);
+
+            // then
+            actualEventV1ProcessingDependencyException.Should()
+                .BeEquivalentTo(expectedEventV1ProcessingDependencyException);
+
+            this.eventV1ServiceMock.Verify(service =>
+                service.ModifyEventV1Async(It.IsAny<EventV1>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedEventV1ProcessingDependencyException))),
+                        Times.Once);
+
+            this.eventV1ServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
