b3e5f924cb1ab507f9dc0a81bab7152f2c02c927
Abdulsamad Osunlana <osunlanaabdulsamad@gmail.com>
2025-05-23 03:33:19 +0100

    ShouldThrowDependencyExceptionOnModifyIfDependencyExceptionOccursAndLogItAsync -> PASS

--- EventHighway.Core.Tests.Unit/Services/Orchestrations/Events/V1/EventV1OrchestrationServiceTests.Exceptions.Modify.cs
+++ EventHighway.Core.Tests.Unit/Services/Orchestrations/Events/V1/EventV1OrchestrationServiceTests.Exceptions.Modify.cs
@@ -58,5 +58,51 @@ namespace EventHighway.Core.Tests.Unit.Services.Orchestrations.Events.V1
             this.eventAddressV1ProcessingServiceMock.VerifyNoOtherCalls();
             this.eventCallV1ProcessingServiceMock.VerifyNoOtherCalls();
         }
+
+        [Theory]
+        [MemberData(nameof(EventV1DependencyExceptions))]
+        public async Task ShouldThrowDependencyExceptionOnModifyIfDependencyExceptionOccursAndLogItAsync(
+            Xeption dependencyException)
+        {
+            // given
+            EventV1 someEventV1 = CreateRandomEventV1();
+
+            var expectedEventV1OrchestrationDependencyException =
+                new EventV1OrchestrationDependencyException(
+                    message: "Event dependency error occurred, contact support.",
+                    innerException: dependencyException.InnerException as Xeption);
+
+            this.eventV1ProcessingServiceMock.Setup(service =>
+                service.ModifyEventV1Async(It.IsAny<EventV1>()))
+                    .ThrowsAsync(dependencyException);
+
+            // when
+            ValueTask<EventV1> modifyEventV1Task =
+                this.eventV1OrchestrationService.ModifyEventV1Async(
+                    someEventV1);
+
+            EventV1OrchestrationDependencyException
+                actualEventV1OrchestrationDependencyException =
+                    await Assert.ThrowsAsync<EventV1OrchestrationDependencyException>(
+                        modifyEventV1Task.AsTask);
+
+            // then
+            actualEventV1OrchestrationDependencyException.Should()
+                .BeEquivalentTo(expectedEventV1OrchestrationDependencyException);
+
+            this.eventV1ProcessingServiceMock.Verify(broker =>
+                broker.ModifyEventV1Async(It.IsAny<EventV1>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedEventV1OrchestrationDependencyException))),
+                        Times.Once);
+
+            this.eventV1ProcessingServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.eventAddressV1ProcessingServiceMock.VerifyNoOtherCalls();
+            this.eventCallV1ProcessingServiceMock.VerifyNoOtherCalls();
+        }
     }
 }
