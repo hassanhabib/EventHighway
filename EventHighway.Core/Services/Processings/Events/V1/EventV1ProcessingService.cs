89953feddf9837b08a23d5d57f8eff5dca94e159
Abdulsamad Osunlana <osunlanaabdulsamad@gmail.com>
2025-05-23 02:59:09 +0100

    ShouldThrowValidationExceptionOnModifyIfEventV1IsNullAndLogItAsync -> PASS

--- EventHighway.Core/Services/Processings/Events/V1/EventV1ProcessingService.cs
+++ EventHighway.Core/Services/Processings/Events/V1/EventV1ProcessingService.cs
@@ -50,10 +50,13 @@ namespace EventHighway.Core.Services.Processings.Events.V1
                 eventV1.ScheduledDate < now);
         });
 
-        public async ValueTask<EventV1> ModifyEventV1Async(EventV1 eventV1)
+        public ValueTask<EventV1> ModifyEventV1Async(EventV1 eventV1) =>
+        TryCatch(async () =>
         {
+            ValidateEventV1IsNotNull(eventV1);
+
             return await this.eventV1Service.ModifyEventV1Async(eventV1);
-        }
+        });
 
         public ValueTask<EventV1> RemoveEventV1ByIdAsync(Guid eventV1Id) =>
         TryCatch(async () =>
